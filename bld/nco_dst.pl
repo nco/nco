96109904d912f64036cfab1f9d05185a69c6ced7
Charlie Zender <zender@uci.edu>
1999-02-26 01:57:20 +0000

    *** empty log message ***

--- bld/nco_dst.pl
+++ bld/nco_dst.pl
@@ -1,20 +1,20 @@
 #!/contrib/bin/perl
 				
-my $RCS_Header='$Header: /data/zender/nco_20150216/nco/bld/nco_dst.pl,v 1.3 1999-01-29 19:51:11 zender Exp $';
+my $RCS_Header='$Header: /data/zender/nco_20150216/nco/bld/nco_dst.pl,v 1.4 1999-02-26 01:57:20 zender Exp $';
 
 # Purpose: Perform NCO distributions
 
 # Usage:
 # Export tagged, public versions
-# /home/zender/nc/nco_dst.pl --dbg=2 --cln nco1_1_21
-# /home/zender/nc/nco_dst.pl --dbg=2 --cln --cgd_cnt nco1_1_21
-# /home/zender/nc/nco_dst.pl --dbg=2 --cln --wnt_cnt nco1_1_21
-# /home/zender/nc/nco_dst.pl --dbg=2 --cln --cray_prs nco1_1_21
-# /home/zender/nc/nco_dst.pl --dbg=2 --cln --nst_all nco1_1_21
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln nco1_1_22
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --cgd_cnt nco1_1_22
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --wnt_cnt nco1_1_22
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --cray_prs nco1_1_22
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --nst_all nco1_1_22
 
 # Export daily snapshot
-# /home/zender/nc/nco_dst.pl --dbg=2 
-# /home/zender/nc/nco_dst.pl --dbg=1 --cln --nst
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=1 --cln --nst
 
 use strict; # Protect all namespaces; this produces many errors with non-expert code
 use File::Basename; # Parses filenames
@@ -48,9 +48,9 @@ my $True=1;
 
 my $CVSROOT='/home/zender/cvs';
 my $PVM_ARCH=$ENV{'PVM_ARCH'};
-my $RCS_Date='$Date: 1999-01-29 19:51:11 $';
-my $RCS_Id='$Id: nco_dst.pl,v 1.3 1999-01-29 19:51:11 zender Exp $';
-my $RCS_Revision='$Revision: 1.3 $';
+my $RCS_Date='$Date: 1999-02-26 01:57:20 $';
+my $RCS_Id='$Id: nco_dst.pl,v 1.4 1999-02-26 01:57:20 zender Exp $';
+my $RCS_Revision='$Revision: 1.4 $';
 my $cln=$True; # GNU standard Makefile option `clean'
 my $dbg_lvl=0;
 my $dst_cln=$False; # GNU standard Makefile option `distclean'
