15da2d3ba907d54b4f7fc582f3a64c16065aeae5
Charlie Zender <zender@uci.edu>
1999-04-05 00:41:02 +0000

    *** empty log message ***

--- bld/nco_dst.pl
+++ bld/nco_dst.pl
@@ -1,17 +1,17 @@
 #!/contrib/bin/perl
 				
-my $RCS_Header='$Header: /data/zender/nco_20150216/nco/bld/nco_dst.pl,v 1.5 1999-04-05 00:32:12 zender Exp $';
+my $RCS_Header='$Header: /data/zender/nco_20150216/nco/bld/nco_dst.pl,v 1.6 1999-04-05 00:41:02 zender Exp $';
 
 # Purpose: Perform NCO distributions
 
 # Usage:
 # Export tagged, public versions
-# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln nco1_1_22
-# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --cgd_cnt nco1_1_22
-# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --wnt_cnt nco1_1_22
-# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --wnt_prs nco1_1_22
-# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --cray_prs nco1_1_22
-# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --nst_all nco1_1_22
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln nco1_1_23
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --cgd_cnt nco1_1_23
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --wnt_cnt nco1_1_23
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --wnt_prs nco1_1_23
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --cray_prs nco1_1_23
+# /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 --cln --nst_all nco1_1_23
 
 # Export daily snapshot
 # /home/zender/nc/nco/bld/nco_dst.pl --dbg=2 
@@ -49,9 +49,9 @@ my $True=1;
 
 my $CVSROOT='/home/zender/cvs';
 my $PVM_ARCH=$ENV{'PVM_ARCH'};
-my $RCS_Date='$Date: 1999-04-05 00:32:12 $';
-my $RCS_Id='$Id: nco_dst.pl,v 1.5 1999-04-05 00:32:12 zender Exp $';
-my $RCS_Revision='$Revision: 1.5 $';
+my $RCS_Date='$Date: 1999-04-05 00:41:02 $';
+my $RCS_Id='$Id: nco_dst.pl,v 1.6 1999-04-05 00:41:02 zender Exp $';
+my $RCS_Revision='$Revision: 1.6 $';
 my $cln=$True; # GNU standard Makefile option `clean'
 my $dbg_lvl=0;
 my $dst_cln=$False; # GNU standard Makefile option `distclean'
