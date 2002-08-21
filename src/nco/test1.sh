3025e0b61c7797914126f6b7df3d9b7922b6c2f7
Rorik Peterson <ffrap1@uaf.edu>
2002-08-21 18:23:48 +0000

    add testing stuff for autoconf

---
+++ src/nco/test1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+test="test1.sh"
+program="ncks"
+datfile="nco_tst.nc"
+datdir="../../data"
+options="-R -p ftp://dust.ps.uci.edu/pub/zender/nco -l $datdir $datfile"
+
+if test -f $datdir/$datfile; then
+   printf "$test: Data file \"$datfile\" already exists locally\n" ; 
+else 
+  exec $program $options
+  if [ $? != 0 ]; then 
+     printf "WARNING: Unable to retrieve ftp://dust.ps.uci.edu/pub/zender/nco/nco_tst.nc required for self-test, possible problem with getting through your firewall? Manually download and install in directory ../data to continue self test...\n"
+  exit 1
+  fi
+fi
+exit 0
