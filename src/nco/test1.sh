0a93f1ba2d17c7ee3f8e35d87b10e4730e2d322f
Charlie Zender <zender@uci.edu>
2002-12-02 16:23:15 +0000

    *** empty log message ***

--- src/nco/test1.sh
+++ src/nco/test1.sh
@@ -3,14 +3,14 @@ test="test1.sh"
 program="ncks"
 datfile="nco_tst.nc"
 datdir="../../data"
-options="-R -p ftp://dust.ps.uci.edu/pub/zender/nco -l $datdir $datfile"
+options="-R -p ftp://dust.ess.uci.edu/pub/zender/nco -l $datdir $datfile"
 
 if test -f $datdir/$datfile; then
    printf "$test: Data file \"$datfile\" already exists locally\n" ; 
 else 
   exec $program $options
   if [ $? != 0 ]; then 
-     printf "WARNING: Unable to retrieve ftp://dust.ps.uci.edu/pub/zender/nco/nco_tst.nc required for self-test, possible problem with getting through your firewall? Manually download and install in directory ../data to continue self test...\n"
+     printf "WARNING: Unable to retrieve ftp://dust.ess.uci.edu/pub/zender/nco/nco_tst.nc required for self-test, possible problem with getting through your firewall? Manually download and install in directory ../data to continue self test...\n"
   exit 1
   fi
 fi
