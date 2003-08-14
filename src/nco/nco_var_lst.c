40f217f38c4df5462dbd1311a18b5bdbb2830b1f
Henry Butowsky <henryb@hush.com>
2003-08-14 14:07:00 +0000

    Added nco_var_lst_mk_meta() and nco_var_meta_search()

--- src/nco/nco_var_lst.c
+++ src/nco/nco_var_lst.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_var_lst.c,v 1.15 2003-08-02 23:26:57 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_var_lst.c,v 1.16 2003-08-14 14:07:00 hmb Exp $ */
 
 /* Purpose: Variable list utilities */
 
@@ -57,6 +57,207 @@ nco_var_lst_mk /* [fnc] Create variable extraction list */
   return xtr_lst;
 
 } /* end nco_var_lst_mk() */
+
+
+
+nm_id_sct * /* O [sct] Variable extraction list */
+nco_var_lst_mk_meta /* [fnc] Create variable extraction list using regular expressions*/
+(const int nc_id, /* I [enm] netCDF file ID */
+ const int nbr_var, /* I [nbr] Number of variables in input file */
+ CST_X_PTR_CST_PTR_CST_Y(char,var_lst_in), /* I [sng] User-specified list of variable names */
+ const bool PROCESS_ALL_COORDINATES, /* I [flg] Process all coordinates */
+ int * const nbr_xtr) /* I/O [nbr] Number of variables in current extraction list */
+{
+  /* Purpose: Create variable extraction list with or without regular expressions*/
+
+  int idx;
+  int jdx;
+  int rcd=NC_NOERR; /* [rcd] Return code */
+  int sng_lng;
+  int srch_sng_lng=0; /* size of srch sng */
+  int nbr_tmp=0;
+  int nbr_match;
+
+  char *srch_sng=NULL;  /* all var names concatenated WITH \n\0 as a separator */
+  char *var_sng;
+
+  nm_id_sct *xtr_lst=NULL; /* xtr_lst may bealloc()'d from NULL with -c option */
+  nm_id_sct *in_lst=NULL; 
+  bool *in_bool=NULL;
+  
+
+
+  /* create list of all vars in input file */
+
+  char var_nm[NC_MAX_NAME];
+    
+  in_lst=(nm_id_sct *)nco_malloc(nbr_var*sizeof(nm_id_sct));
+    for(idx=0;idx<nbr_var;idx++){
+      /* Get name of each variable. */
+      (void)nco_inq_varname(nc_id,idx,var_nm);
+      in_lst[idx].nm=(char *)strdup(var_nm);
+      in_lst[idx].id=idx;
+    } /* end loop over idx */
+
+    /* return all variables if .. */
+    if( *nbr_xtr==0 && !PROCESS_ALL_COORDINATES ) {
+      *nbr_xtr=nbr_var;
+      return in_lst;
+    }      
+      
+  /* intialize and alloc  bool array to all False */
+  in_bool=(bool *)nco_calloc(nbr_var,sizeof(bool));
+  
+  
+  
+  /* create search string from list */
+  for(idx=0;idx<nbr_var;idx++){
+    sng_lng=strlen(in_lst[idx].nm);
+    srch_sng=(char*)nco_realloc(srch_sng,(srch_sng_lng+sng_lng+2)*sizeof(char));
+    strncpy(srch_sng+srch_sng_lng,in_lst[idx].nm,sng_lng);
+     srch_sng_lng=srch_sng_lng+sng_lng+2;
+    *(srch_sng+srch_sng_lng-2)='\n';
+    *(srch_sng+srch_sng_lng-1)='\0';
+    }    /* end loop over idx */
+  
+
+  /* loop through var_lst_in */
+
+  for(idx=0 ; idx<*nbr_xtr ; idx++) {
+  
+     var_sng=var_lst_in[idx];
+     /* if var_sng is  a regular expressiom */
+     if ( strpbrk(var_sng,".*^$\[]()<>+?|")  ) {
+       nbr_match=nco_var_meta_search(nbr_var,srch_sng,var_sng,in_bool);
+       if( nbr_match==0)  
+        	(void)fprintf(stdout,"%s: WARNING: regular expression \"%s\" doesn't match any variables\n",prg_nm_get(),var_sng); 
+       continue;
+     }   
+
+    
+    /* normal variable -- search through var array */
+     for(jdx=0; jdx<nbr_var; jdx++)
+       if( !strcmp(var_sng,in_lst[jdx].nm)) break;
+    
+       /* no match -- die gently */
+     if(jdx==nbr_var) {
+	(void)fprintf(stdout,"%s: ERROR nco_var_lst_mk() reports user-specified variable \"%s\" is not in input file\n",prg_nm_get(),var_sng); 
+        nco_exit(EXIT_FAILURE);
+      }else{
+      in_bool[jdx]=True;
+     }
+            
+  }
+
+  
+  /* create final list of vars using var list and bool array */
+
+  /* malloc xtr_lst to maximium size(nbr_var )  */
+  xtr_lst=(nm_id_sct *)nco_malloc(nbr_var*sizeof(nm_id_sct));
+  nbr_tmp=0;
+
+  for(idx=0 ; idx<nbr_var;idx++) {
+
+    /* copy var to output array */
+    if(in_bool[idx]) {
+      xtr_lst[nbr_tmp].nm=strdup(in_lst[idx].nm);
+      xtr_lst[nbr_tmp].id =in_lst[idx].id;
+      nbr_tmp++; 
+    } 
+    (void)nco_free(in_lst[idx].nm);
+         
+  }
+     /*realloc to actual size */  
+  xtr_lst=(nm_id_sct *)nco_realloc(xtr_lst,nbr_tmp*sizeof(nm_id_sct));
+
+  (void)nco_free(in_lst);
+  (void)nco_free(in_bool);
+  (void)nco_free(srch_sng);
+
+  *nbr_xtr=nbr_tmp;    
+  return xtr_lst;
+
+} /* end nco_var_lst_mk_meta() */
+
+
+int /* O number of matches found */
+nco_var_meta_search  /* search for pattern matches in the var string list */
+(int nbr_var,        /* I number of vars in srch_sng and size of in_bool */
+char *srch_sng,     /* I concatentaed list of vars */
+char *rexp,          /* I regular expression pattern */
+bool *in_bool)       /* O matched vars holder */
+{
+  /* if regular expression doesn't compile then prog crashes out */
+  int idx;
+  int err_no;
+  int cflags;
+  int eflags;
+  int nbr_mtch=0;
+  char *cp;
+  
+  size_t no_sub;
+  
+  regmatch_t *result;
+  regex_t *r;
+
+  r = (regex_t *) malloc(sizeof(regex_t));
+
+  /* choose the RE_SYNTAX_POSIX_EXTENDED regular expression type */
+  cflags=(REG_EXTENDED | REG_NEWLINE);
+  /* set execution flags */
+  eflags=0;
+
+  /* compile regular expression */
+  
+  if((err_no=regcomp(r,rexp ,cflags))!=0) /* Compile the regex */
+    {
+      char *err_sng;  
+    /* POSIX regcomp return error codes. */
+
+      switch(err_no) {
+      case REG_BADPAT:      err_sng="Invalid pattern."; break;  
+      case REG_ECOLLATE:    err_sng="Not implemented."; break;
+      case REG_ECTYPE:	    err_sng="Invalid character class name." ;break;
+      case REG_EESCAPE:	    err_sng="Trailing backslash."; break;
+      case REG_ESUBREG:	    err_sng="Invalid back reference."; break ;
+      case REG_EBRACK:	    err_sng="Unmatched left bracket."; break ;
+      case REG_EPAREN:	    err_sng=" Parenthesis imbalance."; break ;
+      case REG_EBRACE:	    err_sng=" Unmatched \{.  "; break ;
+      case REG_BADBR:	    err_sng=" Invalid contents of { }."; break ;
+      case REG_ERANGE:	    err_sng=" Invalid range end."; break ;
+      case REG_ESPACE:	    err_sng=" Ran out of memory."; break ;
+      case REG_BADRPT:	    err_sng=" No preceding re for repetition op"; break ;
+      default:              err_sng="Invalid pattern."; break;  
+      }
+
+
+ (void)fprintf(stdout,"%s: ERROR nco_var_meta_search() error in regular expression \"%s\" %s \n",prg_nm_get(),rexp, err_sng); 
+    nco_exit(EXIT_FAILURE);
+    }
+
+   no_sub = r->re_nsub+1; /* How many matches are there in a line? */
+
+
+  /* search string */
+   result = (regmatch_t *) malloc(sizeof(regmatch_t) * no_sub);
+
+   cp=srch_sng;
+   /* search each of the var strings for matches */
+   for(idx=0; idx<nbr_var ;idx++){  
+     if( !regexec(r, cp, no_sub, result, eflags))
+       { in_bool[idx]=True;
+         nbr_mtch++;
+       }
+      cp+=strlen(cp)+1; 
+   }
+
+  regfree(r); /* Free the regular expression data structure */
+  (void *)nco_free(r);
+  (void *)nco_free(result);
+
+   return nbr_mtch;
+   
+}
 
 nm_id_sct * /* O [sct] Extraction list */
 nco_var_lst_xcl /* [fnc] Convert exclusion list to extraction list */
