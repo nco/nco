83b826fb338edd24bb1f6a3229a0c9761f2b0405
Charlie Zender <zender@uci.edu>
2003-12-23 18:42:36 +0000

    *** empty log message ***

--- doc/dods.sh
+++ doc/dods.sh
@@ -1,4 +1,4 @@
-# $Header: /data/zender/nco_20150216/nco/doc/dods.sh,v 1.1 2003-12-23 18:39:20 zender Exp $
+# $Header: /data/zender/nco_20150216/nco/doc/dods.sh,v 1.2 2003-12-23 18:42:36 zender Exp $
 
 # Purpose: Demonstrate how to install DODS prior to building NCO as DODS-enabled clients
 
@@ -17,20 +17,21 @@ wget "ftp://ftp.unidata.ucar.edu/pub/dods/DODS-3.4/RC6/source/DODS*.tar.gz"
 tar xvzf DODS-dap-3.4.?.tar.gz
 tar xvzf DODS-nc3-dods-3.4.?.tar.gz
 tar xvzf DODS-packages-3.4.?.tar.gz
+
 # Go to source directory
 cd DODS
-#dods_root=/usr/local # For server (rather than private) installs
-dods_root=`pwd` 
+# Set installation directory
+#DODS_ROOT=/usr/local # For server (rather than private) installs
+DODS_ROOT=`pwd` 
 # Set compiler environment variables CC and CXX before executing this
 # You cannot do it on the command line like with NCO 
 # On Linux, it defaults to gcc and g++
-./configure --prefix=${dods_root}
+./configure --prefix=${DODS_ROOT}
 # Build necessary libraries and install them in /usr/local
 make
-# This fails because we did not extract all DODS source, and that's OK
+# This 'make' fails because we did not extract all DODS source, and that's OK
 # If it works then do a 
 # make install
-# Always keep the DODS libraries in ${dods_root} so NCO can find them
-# and in the darkness bind them.
-printf "DODS-enabled NCO may now be built. To do so, set the
-environment variable\nDODS_ROOT=$dods_root\nand then re-build NCO."
+
+# Keep DODS libraries in ${DODS_ROOT} so NCO finds them and in the darkness binds them 
+printf "DODS-enabled NCO may now be built. To do so, set the environment variable\nDODS_ROOT=$DODS_ROOT\nand then re-build NCO.\n"
