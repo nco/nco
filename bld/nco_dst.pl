893d86bb886b3ec06c3fdbc607412be8a57b841b
Charlie Zender <zender@uci.edu>
2004-05-21 18:11:42 +0000

    *** empty log message ***

--- bld/nco_dst.pl
+++ bld/nco_dst.pl
@@ -6,17 +6,17 @@
 # Usage:
 # Export tagged, public versions
 
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --bld --cln nco-2_9_3 # Build, do not install
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --nst_all nco-2_9_3 # Install, do not build
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --bld --cln --nst_all nco-2_9_3 # Build and install
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --acd_cnt nco-2_9_3
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --acd_prs nco-2_9_3
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --cgd_cnt nco-2_9_3
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --cray_prs nco-2_9_3
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --bbl_cnt nco-2_9_3
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --blk_cnt nco-2_9_3
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --dat_cnt nco-2_9_3
-# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --ute_prs nco-2_9_3
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --bld --cln nco-2_9_4 # Build, do not install
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --nst_all nco-2_9_4 # Install, do not build
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --bld --cln --nst_all nco-2_9_4 # Build and install
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --acd_cnt nco-2_9_4
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --acd_prs nco-2_9_4
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --cgd_cnt nco-2_9_4
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --cray_prs nco-2_9_4
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --bbl_cnt nco-2_9_4
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --blk_cnt nco-2_9_4
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --dat_cnt nco-2_9_4
+# ${HOME}/nco/bld/nco_dst.pl --dbg=2 --cln --ute_prs nco-2_9_4
 
 # Export daily snapshot
 # ${HOME}/nco/bld/nco_dst.pl --dbg=2 
@@ -30,7 +30,7 @@ BEGIN{
     unshift @INC,$ENV{'HOME'}.'/perl'; # Location of csz.pl and DBG.pm HaS98 p. 170
 } # end BEGIN
 
-my $CVS_Header='$Header: /data/zender/nco_20150216/nco/bld/nco_dst.pl,v 1.128 2004-05-05 00:47:18 zender Exp $';
+my $CVS_Header='$Header: /data/zender/nco_20150216/nco/bld/nco_dst.pl,v 1.129 2004-05-21 18:11:41 zender Exp $';
 
 # Specify modules
 use strict; # Protect all namespaces
@@ -67,9 +67,9 @@ my ($rsh_cmd,$rcp_cmd,$cp_cmd,$rm_cmd,$mkdir_cmd,$cvs_cmd);
 my $False=0;
 my $True=1;
 
-my $CVS_Date='$Date: 2004-05-05 00:47:18 $';
-my $CVS_Id='$Id: nco_dst.pl,v 1.128 2004-05-05 00:47:18 zender Exp $';
-my $CVS_Revision='$Revision: 1.128 $';
+my $CVS_Date='$Date: 2004-05-21 18:11:41 $';
+my $CVS_Id='$Id: nco_dst.pl,v 1.129 2004-05-21 18:11:41 zender Exp $';
+my $CVS_Revision='$Revision: 1.129 $';
 my $CVSROOT='zender@cvs.sf.net:/cvsroot/nco'; # CVS repository
 my $HOME=$ENV{'HOME'};
 my $HOST=$ENV{'HOST'};
