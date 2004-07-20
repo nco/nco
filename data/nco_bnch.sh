6e3e43f926fa3f52faaa61b630ca81f091d66166
Charlie Zender <zender@uci.edu>
2004-07-20 00:40:51 +0000

    *** empty log message ***

--- data/nco_bnch.sh
+++ data/nco_bnch.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# $Header: /data/zender/nco_20150216/nco/data/nco_bnch.sh,v 1.2 2004-07-20 00:18:24 zender Exp $
+# $Header: /data/zender/nco_20150216/nco/data/nco_bnch.sh,v 1.3 2004-07-20 00:40:51 zender Exp $
 
 # Purpose: Benchmark NCO performance
 
 # Usage:
 
 # Create file with dimension sizes
-ncgen -b -o ${DATA}/mie/big.nc ~/nco/data/big.cdl
+ncgen -b -o ~/nco/data/big.nc ~/nco/data/big.cdl
 
 # Create file with decadal differences in dimension sizes
 ncap -D 3 -O \
@@ -20,20 +20,34 @@ ncap -D 3 -O \
 -s "wvl_1e6[wvl_1e6]=1.0f" \
 -s "wvl_1e7[wvl_1e7]=1.0f" \
 -s "wvl_1e8[wvl_1e8]=1.0f" \
-${DATA}/mie/big.nc ${DATA}/mie/big.nc
+~/nco/data/big.nc ${DATA}/tmp/big.nc
 
-# Benchmark arithmetic operations on large files
-timex ncwa -O -a wvl_1e8 -v wvl_1e8 ${DATA}/mie/big.nc ${DATA}/mie/foo.nc;ncks -H ${DATA}/mie/foo.nc
-date;ncwa -O ${DATA}/mie/big.nc ${DATA}/mie/foo.nc;date;ncks -H ${DATA}/mie/foo.nc
-date;ncwa -O -a wvl_1e8 -v wvl_1e8 ${DATA}/mie/big.nc ${DATA}/mie/foo.nc;date;ncks -H ${DATA}/mie/foo.nc
+# Benchmarks for ncwa 
+# ncwa_b1:
+# Average 1-D arrays of multiple sizes
+timex ncwa -O -a wvl_1e8 -v wvl_1e8 ${DATA}/tmp/big.nc ${DATA}/tmp/foo.nc;ncks -H ${DATA}/tmp/foo.nc
+date;ncwa -O ${DATA}/tmp/big.nc ${DATA}/tmp/foo.nc;date;ncks -H ${DATA}/tmp/foo.nc
+date;ncwa -O -a wvl_1e8 -v wvl_1e8 ${DATA}/tmp/big.nc ${DATA}/tmp/foo.nc;date;ncks -H ${DATA}/tmp/foo.nc
 
 # Results: 
 # elnino gcc 3.3.4 with restrict keyword     20040719: 11m31s
 # elnino gcc 3.3.4 without restrict keyword  20040719: 10m20s
-# esmf04m xlc 6.x with restrict keyword      20040719: 22s
+# esmf04m xlc 6.x with restrict keyword      20040719: 25s
 # esmf04m xlc 6.x without restrict keyword   20040719: 25s
 # 2^32=4294967296
 # 2^31=2147483648
 # 10^9=1000000000
 # 10^8= 100000000
 
+# Benchmarks for ncra:
+# Create file with multiple long record arrays
+ncap -D 3 -O \
+-s "one[wvl_1e7]=1.0f" \
+-s "two[wvl_1e7]=2.0f" \
+-s "three[wvl_1e7]=3.0f" \
+-s "four[wvl_1e7]=4.0f" \
+~/nco/data/big.nc ${DATA}/tmp/big.nc
+# Running average with one thread
+date;ncra -O -t 1 ${DATA}/tmp/big.nc ${DATA}/tmp/foo.nc;date
+# Running average with four threads
+date;ncra -O -t 1 ${DATA}/tmp/big.nc ${DATA}/tmp/foo.nc;date
