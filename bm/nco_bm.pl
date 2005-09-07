d649be37ab7597526fd0dd07b59b265cf4b89412
Charlie Zender <zender@uci.edu>
2005-09-07 23:19:05 +0000

    *** empty log message ***

--- bm/nco_bm.pl
+++ bm/nco_bm.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# $Header: /data/zender/nco_20150216/nco/bm/nco_bm.pl,v 1.74 2005-09-07 23:18:03 zender Exp $
+# $Header: /data/zender/nco_20150216/nco/bm/nco_bm.pl,v 1.75 2005-09-07 23:19:05 zender Exp $
 
 # Usage:  usage(), below, has more information
 # ~/nco/bm/nco_bm.pl # Tests all operators
@@ -1114,7 +1114,7 @@ if ($mpi_prc == 0 || ($mpi_prc > 0 && $opr_rgr_mpi =~ /$opr_nm/)) {
 	$tst_cmd[1]="ncbo $omp_flg -C -h -O $nco_D_flg -v rec_var_flt_mss_val_dbl in.nc $foo_avg_fl $outfile";
 	$tst_cmd[2]="ncks -C -H -d time,3 -s '%f' -v rec_var_flt_mss_val_dbl $outfile";
 	$dsc_sng="Difference which tests broadcasting and changing variable IDs";
-	if($mpi_prc > 0) $dsc_sng+="Known bug with mpncbo: fix is TODO nco579";
+	if($mpi_prc > 0){$dsc_sng+="Known bug with mpncbo: fix is TODO nco579";}
 	$nsr_xpc= -1.0 ; 
 	&go();
 
