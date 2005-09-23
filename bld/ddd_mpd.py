472f9ff9fbefea8bd28046987bb2e6eb0c5a58dc
Daniel Wang <danielw@slac.stanford.edu>
2005-09-23 22:29:24 +0000

    ddd_mpd.py is a simple Python script that starts ddd sessions for each process of one job listed in mpdlistjobs.  Currently MPICH2/mpd only.

---
+++ bld/ddd_mpd.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# spawns a ddd session for each one found in mpilistjobs
+# nco-specific: sends SIGUSR1 to nodeid>0 processes
+# $id$ 
+
+
+from  os import environ, getuid, getpid, path, getcwd, popen3
+import os
+
+class Job:
+    pass
+
+def newJob(jid, username, host, pid, sid, rank, path):
+    job = Job()
+    job.jobid = jid
+    job.username = username
+    job.host = host
+    job.pid = pid
+    job.sid = sid
+    job.rank = rank
+    job.pgm = path
+    return job
+
+def readJobInfo(jobtext):
+    job = Job()
+    #print "got job ", jobtext
+    lines = jobtext.split("\n")
+    for line in lines:
+        pair = line.split("=")
+        if len(pair) > 1:
+            key,value = pair
+            #print key.strip(),value.strip()
+            exec "job."+key.strip()+"="+"\""+value.strip()+"\""
+    return job;
+
+def readJobListMPD():
+    joblist = []
+    jobI, jobO, jobE = popen3("mpdlistjobs")
+    allinfo = jobO.read()
+    for jobtext in allinfo.split("\n\n"):
+        joblist.append(readJobInfo(jobtext))
+    return joblist
+
+def makeSampleJobList(): ## dummy for testing
+    joblist = []
+    
+    joblist.append(newJob( "21@dirt_3734", "wangd", "dirt",
+                              "11931", "11926", "0",
+                              "/home/wangd/nco/nco/mpi_bin/mpncwa"))
+    joblist.append(newJob( "21@dirt_3734", "wangd", "dirt",
+                              "11929", "11927", "1",
+                              "/home/wangd/nco/nco/mpi_bin/mpncwa"))
+    joblist.append(newJob( "21@dirt_3734", "wangd", "dirt",
+                              "11930", "11928", "2",
+                              "/home/wangd/nco/nco/mpi_bin/mpncwa"))
+    # next two jobs should be culled
+    joblist.append(newJob( "10@dirt_3734", "somebody", "dirt",
+                              "112", "110", "0",
+                              "/badpath/mpncbo"))
+    joblist.append(newJob( "10@dirt_3734", "somebody", "dirt",
+                              "119", "111", "1",
+                              "/badpath/mpncbo"))
+    joblist.append(newJob( "24@dirt_3734", "wangd", "dirt",
+                              "11951", "11946", "0",
+                              "/home/wangd/nco/nco/mpi_bin/mpncbo"))
+    joblist.append(newJob( "24@dirt_3734", "wangd", "dirt",
+                              "11949", "11947", "1",
+                              "/home/wangd/nco/nco/mpi_bin/mpncbo"))
+    joblist.append(newJob( "24@dirt_3734", "wangd", "dirt",
+                              "11950", "11948", "2",
+                              "/home/wangd/nco/nco/mpi_bin/mpncbo"))
+
+    return joblist
+
+
+def cutoutJobs(joblist, jobids, paths):
+    print "There seems to be more than one job in MPD.  Which would you like?"
+    pick = ""
+    index = -1
+    while not pick in jobids:
+        for i in range(len(jobids)):
+            print i,jobids[i], paths[i]
+        print "which would you like(0..",len(jobids)-1,")?",
+        typed = raw_input()
+        try:
+            index = int(typed)
+        except ValueError:
+            pass
+        if index not in range(len(jobids)):    
+            print "Sorry, bad choice: ",typed, " Try another."
+        else:
+            pick = jobids[index]
+    print "Chose id:", pick, " path:",paths[index]
+
+    # remove job objects that do not match
+    joblist = filter((lambda j: j.jobid == pick), joblist)
+    return joblist
+
+def spawnDebugger(progname, joblist, debug):
+    shI, shO, shE = popen3("sh")
+
+    children = filter(lambda j: j.rank != "0", joblist)
+    for c in children: # resume the children first
+        s = "kill -USR1 %s\n" % (c.pid)
+        if not debug: shI.write(s)
+        else: print s,
+    print "Node 0 is pid:",filter(lambda j: j.rank == "0",joblist)[0].pid
+    shI.write("\n")
+    for job in joblist:
+        try:
+            #print job.jobid, job.pid, job.rank, job.pgm
+            dummy = job.pgm
+            dummy = job.pid
+            s = "%s %s %s &\n" % (progname, job.pgm, job.pid)
+            if not debug: shI.write(s)
+            #else:
+            print s,
+            
+        except AttributeError:
+            continue
+
+    shI.close()
+    print shO.read().strip()
+    print shE.read().strip()
+    
+
+def mainthing():
+    joblist = readJobListMPD()
+    #joblist = makeSampleJobList() # for simple testing
+    # now, go ahead and spawn ddd jobs.
+    # for now, just spit out the command line so we don't have to deal
+    # with the process management. *sigh*
+    ####print len(joblist)
+    jobids = []
+    paths = []
+    joblist = filter(lambda j: hasattr(j, "jobid"), joblist)
+    myname = os.popen("whoami").read().strip()
+    joblist = filter(lambda j: j.username == myname, joblist)
+    if len(joblist) < 1:
+        print "No acceptable jobs found."
+        return
+    for job in joblist: ## check to see how many jobs there are
+        if not job.jobid in jobids:
+            jobids.append( job.jobid )
+            paths.append( job.pgm )
+    if len(jobids) > 1:
+        joblist = cutoutJobs(joblist, jobids, paths)
+
+    spawnDebugger("ddd", joblist, False) #False for no debug
+        
+
+## -----------------
+## main program body
+## -----------------
+mainthing()
+
+
+    
