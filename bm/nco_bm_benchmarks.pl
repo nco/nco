68ba5ee8e6beb7ca929646637a30c1e98872edbd
Harry Mangalam <hjm@tacgi.com>
2005-11-30 22:58:13 +0000

    changes to benchmark results due to different tests being performed

--- bm/nco_bm_benchmarks.pl
+++ bm/nco_bm_benchmarks.pl
@@ -4,7 +4,7 @@
 # for the NCO benchmark script nco_bm.pl
 # It must maintain Perl semantics for Perl code.
 
-# $Header: /data/zender/nco_20150216/nco/bm/nco_bm_benchmarks.pl,v 1.11 2005-11-30 22:53:02 mangalam Exp $
+# $Header: /data/zender/nco_20150216/nco/bm/nco_bm_benchmarks.pl,v 1.12 2005-11-30 22:58:13 mangalam Exp $
 
 	print "\nINFO: Starting Benchmarks now\n";
 	if($dbg_lvl > 1){print "bm: prefix = $prefix\n";}
@@ -44,7 +44,7 @@
 		$tst_cmd[0] = "ncap -h -O $fl_fmt $nco_D_flg -s  \"nu_var1[time,lev,lat,lon]=d4_01*d4_02*(d4_03^2)-(d4_05/d4_06)\" 	-s \"nu_var2[time,lev,lat,lon]=(d4_13/d4_02)*((d4_03^2)-(d4_05/d4_06))\" -s \"nu_var3[time,lat,lon]=(d3_08*d3_01)-(d3_05^3)-(d3_11*d3_16)\" -s \"nu_var4[time,lat,lon]=(d3_08+d3_01)-((d3_05*3)-d3_11-17.33)\" $in_pth_arg ipcc_dly_T85.nc $outfile";
 		$tst_cmd[1] = "ncwa -O $omp_flg -y sqrt -a lat,lon $outfile $outfile";
 		$tst_cmd[2] = "ncks -C -H -s '%f' -v d2_00 $outfile";
-		$nsr_xpc = "1.604304";
+		$nsr_xpc = "4.024271";
 		go();
 		if($dbg_lvl > 0){print "\n[past benchmark stanza - $dsc_sng]\n";}
 	} else {print "Skipping Benchmark [$opr_nm] - not MPI-ready\n";}
