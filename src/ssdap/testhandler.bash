1a3b330d0004438df71c10542f6ab5b73c3e4750
Daniel Wang <danielw@slac.stanford.edu>
2006-01-25 00:26:32 +0000

    add some files to do server-side processing on opendap -dlw

---
+++ src/ssdap/testhandler.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+bin/dap_nc_handler_hack -L -o dods -r /usr/tmp -e superduperscript11 -c -u http://localhost:8000/cgi/nph_dods/nc/foo_T42.nc -v DAP2/3.5.3 /home/wangd/opendap/aolserver4/servers/aoldap/pages/nc/foo_T42.nc <simplescript.ssdap
+
