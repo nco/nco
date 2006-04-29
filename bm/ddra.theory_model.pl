ba6fe87cbe155eeaaec8f29e4583e2d02245f234
Charlie Zender <zender@uci.edu>
2006-04-29 05:38:08 +0000

    Change $sz_wgt_blk to $sz_avg_blk in ddra.theory_model.pl

--- bm/ddra.theory_model.pl
+++ bm/ddra.theory_model.pl
@@ -12,58 +12,58 @@ use strict;
 # Declare vars for strict
 use vars qw(
 	    $ncbo
-	    $ncwa_atomic_INS $bytes_in $bytes_out $bytes_per_val $ncwa_compute_sec_nowt
-	    $dim_sz $e $ncwa_Icyc_per_el $r $timestamp $INS_per_Int_op $INS_per_FP_op
-	    $ncwa_compute_sec_wt $cpu_clock_freq $disk_bw_read $disk_bw_write $disk_time_in
-	    $disk_time_out $disk_ttl_IO_time $int_coeff $ncwa_est_flops $ncwa_int_INS_nowt $ncwa_int_INS_wt
-	    $ncwa_memcpy_INS_nowt  $ncwa_memcpy_per_el_nowt $ncwa_memcpy_per_el_wt $N
-	    $nbr_files_in $nbr_files_out $nbr_vars $ncwa_NOWT_INS_multiplier $rank $rank @dim_vars
+	    $ncwa_atomic_INS $byt_in $byt_out $byt_per_val $ncwa_compute_sec_nowt
+	    $var_sz $var_idx $ncwa_Icyc_per_el $rnk_idx $timestamp $INS_per_Int_op $INS_per_FP_op
+	    $ncwa_compute_sec_wt $cpu_clock_freq $dsk_bw_rd $dsk_bw_wrt $dsk_tm_in
+	    $dsk_tm_out $dsk_ttl_IO_tm $int_coeff $ncwa_est_flops $ncwa_int_INS_nowt $ncwa_int_INS_wt
+	    $ncwa_memcpy_INS_nowt  $ncwa_memcpy_per_el_nowt $ncwa_memcpy_per_el_wt $lmn_nbr_ttl
+	    $fl_nbr_in $fl_nbr_out $var_nbr $ncwa_NOWT_INS_multiplier $rnk $rnk @dim_vars
 	    $ncwa_ttl_cyc_mlt_nowt $ncwa_ttl_cyc_mlt_wt $ncwa_TOT_CYC_nowt $ncwa_TOT_CYC_wt $ncwa_TOT_INS_multiplier $ncbo_Icyc_per_ins $ncbo_Int_INS_per_el $stl_5km $out_fl_nm $sz_avg_blk
-	    $V_flops $V_int_INS $ncwa_wallclock_sec_nowt $ncwa_wallclock_sec_wt $weighting $wgt_reuse $V_intops
-	    $ncwa_weighting_correction $ncwa_weighting_multiplier $ncwa_WT_INS_multiplier $R_wgt
+	    $V_flops $V_int_INS $ncwa_wallclock_sec_nowt $ncwa_wallclock_sec_wt $wgt_flg $wgt_reuse_flg $V_intops
+	    $ncwa_weighting_correction $ncwa_weighting_multiplier $ncwa_WT_INS_multiplier $rnk_wgt
 	    
-	    $K_Ia  $K_Mu  $F_cnt  $Ia_cnt  $Mu_cnt  $Ms_cnt  $fp_time  $Ittl_cnt $sz_wgt_blk
-	    $Int_time $ncwa_int_time $ncwa_int_count
-	    $MRV $MRV_mlt_fct
+	    $K_Ia  $K_Mu  $F_cnt  $Ia_cnt  $Mu_cnt  $Ms_cnt  $fp_tm  $Ittl_cnt $sz_wgt_blk
+	    $Int_tm $ncwa_int_tm $ncwa_int_count
+	    $MRV_flg $MRV_mlt_fct
 	    );
 
 # Initialization: Variables that user must set/check to generate valid run
 $ncbo                       = 0; # 1 for ncbo routines, 0 for ncwa
-$weighting                  = 1; # 1 for weighting, 0 for NOT weighting (no effect on ncbo)
-$wgt_reuse                  = 0; # 1 for weight re-use (new default), 0 no re-use (old default)
-$stl_5km                    = 0; # 1 if file is stl_5km file, 0 for ipcc file
-$dim_sz                     = 1; # simple initialization for dimension size
-$MRV                        = 0; # Use MRV optimization 
+$wgt_flg                        = 1; # 1 for weighting, 0 for NOT weighting (no effect on ncbo)
+$wgt_reuse_flg                  = 0; # 1 for weight re-use (new default), 0 no re-use (old default)
+$stl_5km                        = 0; # 1 if file is stl_5km file, 0 for ipcc file
+$var_sz                         = 1; # simple initialization for dimension size
+$MRV_flg                        = 0; # Use MRV optimization 
 
 # Variables relating to size and layout of test files
 # These should be set correctly using other than standard files (stl_5km and ipcc)
 if ($stl_5km) {               # lat   lon
-    $rank = @dim_vars         = (2160, 4320);
-    $sz_wgt_blk               = 2160; # size of weight variable (lat)
-    $R_wgt                    = 1; # Rank of weight
-    $sz_avg_blk               = 2160 * 4320; # N sub A in ppr
-    $nbr_vars                 = 8; # nbr of variables (assuming all same size)
+    $rnk = @dim_vars         = (2160, 4320);
+    $sz_wgt_blk              = 2160; # size of weight variable (lat)
+    $rnk_wgt                 = 1; # Rank of weight
+    $sz_avg_blk              = 2160 * 4320; # N sub A in ppr
+    $var_nbr                 = 8; # nbr of variables (assuming all same size)
     $out_fl_nm               = "ddra.stl_5km.";
 } else { # assume ipcc file            lat  lon
-    $rank = @dim_vars         = (8, 32, 128, 256); # Rank vars that are >94% of file
-    $sz_wgt_blk                = 128; # Size of weight variable (lat)
-    $R_wgt                    = 1; # Rank of weight
-    $sz_avg_blk                 = 8 * 32 * 128 * 256; # N sub A in ppr
-    $nbr_vars                 = 32; # for most significant vars in ipcc - 94% of file
+    $rnk = @dim_vars         = (8, 32, 128, 256); # Rank vars that are >94% of file
+    $sz_wgt_blk              = 128; # Size of weight variable (lat)
+    $rnk_wgt                 = 1; # Rank of weight
+    $sz_avg_blk              = 8 * 32 * 128 * 256; # N sub A in ppr
+    $var_nbr                 = 32; # for most significant vars in ipcc - 94% of file
     $out_fl_nm               = "ddra.ipcc.";
 }
 
 # Number of files in and out:
 if ($ncbo) {
-    $nbr_files_in             = 2;  # number of files in - 2 for ncbo, 1 for ncwa
+    $fl_nbr_in             = 2;  # number of files in - 2 for ncbo, 1 for ncwa
     $out_fl_nm .= "ncbo";
 #	$INS_per_Int_op              = 2.0;   #  NCBO - calculated from tacg utility
 } else {
-    $nbr_files_in             = 1;
+    $fl_nbr_in             = 1;
     $out_fl_nm              .= "ncwa";
     # Following calculations imply integer operations are 18--28 times faster than FP
 }
-$nbr_files_out              = 1; # number of files out
+$fl_nbr_out              = 1; # number of files out
 
 # Value of $INS_per_Int_op is not used in this script.
 # Contrary to what I thought before, it seems to be constant across both ncbo and ncwa
@@ -74,12 +74,12 @@ $nbr_files_out              = 1; # number of files out
 # Following vars are set for clay and are relatively stable unless changing machines
 # I retracti my previous estimate--- 
 # I think bonnie estimates work better across range of tests we're doing
-$disk_bw_read               = 63.375e6; # fr disk read bandwidth in bytes/s (bonnie++)
-$disk_bw_write              = 57.865e6; # fr disk write bandwidth in bytes/s (bonnie++)
-#$disk_bw_read               = 100e6; # value from to or from memory - theoretical max
-#$disk_bw_write              = 60e6;  # value from to or from memory - theoretical max
+$dsk_bw_rd               = 63.375e6; # fr disk read bandwidth in byt/s (bonnie++)
+$dsk_bw_wrt              = 57.865e6; # fr disk write bandwidth in byt/s (bonnie++)
+#$dsk_bw_rd               = 100e6; # value from to or from memory - theoretical max
+#$dsk_bw_wrt              = 60e6;  # value from to or from memory - theoretical max
 
-$bytes_per_val              = 4;   #change if using doubles or other non-NC_FLOAT sizes
+$byt_per_val              = 4;   #change if using doubles or other non-NC_FLOAT sizes
 $cpu_clock_freq             = 2e9; # CPU clock in Hz
 
 $V_flops                    = 153e6; # floating point speed derived from direct PAPI measures
@@ -93,10 +93,10 @@ $V_intops                   = 200e6; # int speed from direct measure, using a la
 #$V_flops                    = 1017e6; # floating point speed derived from BOINC measures
 #$V_intops                   = 2147e6; # integer speed derived from BOINC measures
 
-for (my $r=0; $r<$rank; $r++){$dim_sz *= $dim_vars[$r];} # total scalar size of all dimensions
-# $N = total nbr of els - only for those calcs that do not run thru the
-# N calculations in the loop below
-$N = $nbr_vars * $dim_sz;
+for (my $rnk_idx=0; $rnk_idx<$rnk; $rnk_idx++){$var_sz *= $dim_vars[$rnk_idx];} # total scalar size of all dimensions
+# $lmn_nbr_ttl = total nbr of elelments - only for those calcs that do not run thru the
+# Number of elements in all variables counted in loop below
+$lmn_nbr_ttl = $var_nbr * $var_sz;
 
 my $time;
 $timestamp = `date "+%a%d%b%g-%R"`; chomp $timestamp;
@@ -105,66 +105,62 @@ open(GPD, ">$out_fl_nm.$timestamp.gpdata") or die "\nUnable to open GPD file\n";
 
 # this stanza does ncwa for an initial data size of 10MB up to 80.(steps of 8)
 if (!$ncbo) {
-    my $WGT = "weighted";
-    if ($weighting == 0) {$WGT = "unweighted";}
+    my $wgt_sng = "weighted";
+    if ($wgt_flg == 0) {$wgt_sng = "unweighted";}
     
-    print GPD  "#ncwa $WGT \n#         bytes        ops         ops         ops         ops        ops           sec        sec\nCycle   DataSize     Floating    Int_math    Mem_user     Mem_sys   Total_Int   FP_time    Int_time     I/O       Tot_Time\n";
+    print GPD  "#ncwa $wgt_sng \n#         bytes        ops         ops         ops         ops        ops           sec        sec\nCycle   DataSize     Floating    Int_math    Mem_user     Mem_sys   Total_Int   FP_tm      Int_tm       I/O       Tot_tm\n";
     
-    print "#ncwa $WGT \n#         bytes        ops         ops         ops         ops        ops           sec        sec\nCycle   DataSize     Floating    Int_math    Mem_user     Mem_sys   Total_Int   FP_time    Int_time     I/O       Tot_Time\n";
+    print "#ncwa $wgt_sng \n#         bytes        ops         ops         ops         ops        ops           sec        sec\nCycle   DataSize     Floating    Int_math    Mem_user     Mem_sys   Total_Int   FP_tm    Int_tm       I/O         Tot_tm\n";
     
-    for (my $e=1; $e<=$nbr_vars; $e++) {
-	my $dsz = $e * $dim_sz;
+    for (my $var_idx=1; $var_idx<=$var_nbr; $var_idx++) {
+	my $lmn_nbr = $var_idx * $var_sz;
 	# Re-arranged to better match ZeM06 equations
-	if($weighting == 0){ # without weighting (weight reuse value does not matter when their are no weights)
+	if($wgt_flg == 1){ # Weighting
 	    $K_Ia = 1; # fudge for Ia
 	    $K_Mu = 1; # fudge for Mu
-	    $F_cnt = $dsz * (1 + (1/$sz_avg_blk)); # 19a
-	    $Ia_cnt = $K_Ia * ($dsz * ((9 * $rank) + 9 + (4 / $sz_avg_blk))); # 19b
-	    $Mu_cnt = $K_Mu * ($dsz * ((5 * $rank) + 3 + ($bytes_per_val + 2) / $sz_avg_blk)); # 19c
-	    $Ms_cnt = $dsz; # 19d
-	}elsif($weighting == 1){ # with weighting
+	    $F_cnt = $lmn_nbr * (3 + (2/$sz_avg_blk)); # 20a
+	    $Ia_cnt = $K_Ia * ($lmn_nbr * ((22*$rnk) + (5*$rnk_wgt) + 18 + (7/$sz_avg_blk)) + (2*$sz_wgt_blk)); # 20b
+	    $Mu_cnt = $K_Mu * ($lmn_nbr * ((12*$rnk) + (3*$rnk_wgt) + 4 + ($byt_per_val + 4) / $sz_avg_blk) + ($byt_per_val*$sz_wgt_blk)); # 20c
+	    $Ms_cnt = 3*$lmn_nbr; # 20d
+	}else{ # !wgt_flg
 	    $K_Ia = 1; # fudge for Ia
 	    $K_Mu = 1; # fudge for Mu
-	    $F_cnt = $dsz * (3 + (2/$sz_avg_blk)); # 20a
-	    $Ia_cnt = $K_Ia * ($dsz * ((22*$rank) + (5*$R_wgt) + 18 + (7/$sz_avg_blk)) + (2*$sz_wgt_blk)); # 20b
-	    $Mu_cnt = $K_Mu * ($dsz * ((12*$rank) + (3*$R_wgt) + 4 + ($bytes_per_val + 4) / $sz_avg_blk) + ($bytes_per_val*$sz_wgt_blk)); # 20c
-	    $Ms_cnt = 3*$dsz; # 20d
-	}else{ # Error
-	    die "\nInvalid ncwa case\n";
-	} # end else
-	if($weighting == 1 && $wgt_reuse == 1){ # With weight re-use
+	    $F_cnt = $lmn_nbr * (1 + (1/$sz_avg_blk)); # 19a
+	    $Ia_cnt = $K_Ia * ($lmn_nbr * ((9 * $rnk) + 9 + (4 / $sz_avg_blk))); # 19b
+	    $Mu_cnt = $K_Mu * ($lmn_nbr * ((5 * $rnk) + 3 + ($byt_per_val + 2) / $sz_avg_blk)); # 19c
+	    $Ms_cnt = $lmn_nbr; # 19d
+	} # !wgt_flg
+	if($wgt_flg == 1 && $wgt_reuse_flg == 1){ # With weight re-use
 	    # Subtract cost of broadcasting weights
-	    # fxm: fix factors
-	    $Ia_cnt-= $K_Ia * ($dsz * ((22*$rank) + (5*$R_wgt) + 18 + (7/$sz_avg_blk)) + (2*$sz_wgt_blk)); # 20b
-	    $Mu_cnt-= $K_Mu * ($dsz * ((12*$rank) + (3*$R_wgt) + 4 + ($bytes_per_val + 4) / $sz_avg_blk) + ($bytes_per_val*$sz_wgt_blk)); # 20c
-	    $Ms_cnt-= 3*$dsz; # 20d
-	} # end $wgt_reuse
-	if($MRV == 1){
+	    $Ia_cnt-= $K_Ia * ($lmn_nbr * ((4*$rnk) + (5*$rnk_wgt) + 2)); # 12a
+	    $Mu_cnt-= $K_Mu * ($lmn_nbr * ((2*$rnk) + (3*$rnk_wgt) - 1)); # 12b
+	    $Ms_cnt-= $lmn_nbr; # 12c
+	} # end $wgt_reuse_flg
+	if($MRV_flg == 1){
 	    # Subtract collection cost for unweighted averaging
 	    # Subtract twice collection cost for weighted averaging
-	    # fxm: fix factors
-	    if($weighting = 1){$MRV_mlt_fct=2;}else{$MRV_mlt_fct=1;}
-	    $Ia_cnt-=$MRV_mlt_fct*$K_Ia * ($dsz * ((22*$rank) + (5*$R_wgt) + 18 + (7/$sz_avg_blk)) + (2*$sz_wgt_blk)); # 20b
-	    $Mu_cnt-=$MRV_mlt_fct* $K_Mu * ($dsz * ((12*$rank) + (3*$R_wgt) + 4 + ($bytes_per_val + 4) / $sz_avg_blk) + ($bytes_per_val*$sz_wgt_blk)); # 20c
-	    $Ms_cnt-=$MRV_mlt_fct* 3*$dsz; # 20d
-	} # !MRV
+	    if($wgt_flg = 1){$MRV_mlt_fct=2;}else{$MRV_mlt_fct=1;}
+	    $Ia_cnt-=$MRV_mlt_fct*$K_Ia * ($lmn_nbr * (9*$rnk + 4)); # 13a
+	    $Mu_cnt-=$MRV_mlt_fct* $K_Mu * ($lmn_nbr * (5*$rnk - 1)); # 13b
+	    $Ms_cnt-=$MRV_mlt_fct* $lmn_nbr; # 13c
+	} # !MRV_flg
 
-	$bytes_in  = $dsz * $bytes_per_val * $nbr_files_in;
+	$byt_in  = $lmn_nbr * $byt_per_val * $fl_nbr_in;
 	
 	# Averaging (rank reduction) file out size is less than input file size by size of averaging variable - $sz_wgt_blk
-	$bytes_out = ($dsz/$sz_wgt_blk) * $bytes_per_val * $nbr_files_out;
-	$disk_time_in = $bytes_in / $disk_bw_read;
-	$disk_time_out = $bytes_out / $disk_bw_write;
-	$disk_ttl_IO_time = $disk_time_in + $disk_time_out;
-#		printf "bytes in=  %1.4e \$disk_time_in = %1.4e \nbytes out= %1.4e \$disk_time_out = %1.4e \n\$disk_ttl_IO_time =  %1.4e\n", $bytes_in, $disk_time_in, $bytes_out, $disk_time_out, $disk_ttl_IO_time;
-	$fp_time = $F_cnt / $V_flops;
+	$byt_out = ($lmn_nbr/$sz_avg_blk) * $byt_per_val * $fl_nbr_out;
+	$dsk_tm_in = $byt_in / $dsk_bw_rd;
+	$dsk_tm_out = $byt_out / $dsk_bw_wrt;
+	$dsk_ttl_IO_tm = $dsk_tm_in + $dsk_tm_out;
+#		printf "byt in=  %1.4e \$dsk_tm_in = %1.4e \nbyt out= %1.4e \$dsk_tm_out = %1.4e \n\$dsk_ttl_IO_tm =  %1.4e\n", $byt_in, $dsk_tm_in, $byt_out, $dsk_tm_out, $dsk_ttl_IO_tm;
+	$fp_tm = $F_cnt / $V_flops;
 	$Ittl_cnt = $Ia_cnt + $Mu_cnt + $Ms_cnt;
-	$Int_time = $Ittl_cnt / $V_intops; # remember that $V_intops is under some suspicion
-	$time = $disk_ttl_IO_time + $fp_time  + $Int_time;
+	$Int_tm = $Ittl_cnt / $V_intops; # remember that $V_intops is under some suspicion
+	$time = $dsk_ttl_IO_tm + $fp_tm  + $Int_tm;
 	
 	#         IO                IO             FP term                 Int term
-	printf     "%d       %1.3e   %1.3e   %1.3e   %1.3e   %1.3e  %1.3e   %1.3e  %1.3e   %1.3e   %1.3e\n", $e,      $dsz,   $F_cnt,  $Ia_cnt,   $Mu_cnt,  $Ms_cnt,  $Ittl_cnt, $fp_time, $Int_time, $disk_ttl_IO_time,  $time ;
-	printf GPD "%d       %1.3e   %1.3e   %1.3e   %1.3e   %1.3e  %1.3e   %1.3e  %1.3e   %1.3e   %1.3e\n", $e,      $dsz,   $F_cnt,  $Ia_cnt,   $Mu_cnt,  $Ms_cnt,  $Ittl_cnt, $fp_time, $Int_time, $disk_ttl_IO_time,  $time ;
+	printf     "%d       %1.3e   %1.3e   %1.3e   %1.3e   %1.3e  %1.3e   %1.3e  %1.3e   %1.3e   %1.3e\n", $var_idx,      $lmn_nbr,   $F_cnt,  $Ia_cnt,   $Mu_cnt,  $Ms_cnt,  $Ittl_cnt, $fp_tm, $Int_tm, $dsk_ttl_IO_tm,  $time ;
+	printf GPD "%d       %1.3e   %1.3e   %1.3e   %1.3e   %1.3e  %1.3e   %1.3e  %1.3e   %1.3e   %1.3e\n", $var_idx,      $lmn_nbr,   $F_cnt,  $Ia_cnt,   $Mu_cnt,  $Ms_cnt,  $Ittl_cnt, $fp_tm, $Int_tm, $dsk_ttl_IO_tm,  $time ;
     }
 }
 
@@ -175,32 +171,32 @@ Cycle   DataSize     T_fp     T_int    T_I/O      T_tot      Int_ops\n";
     print "#ncbo - NB: FP op counts = DataSize\n
 Cycle   DataSize     T_fp     T_int    T_I/O      T_tot      Int_ops\n";
     
-    for (my $e=1; $e<=$nbr_vars; $e++) {
-	my $dsz = $e * $dim_sz;
-	$bytes_in  = $dsz * $bytes_per_val * $nbr_files_in;
-	$bytes_out = $dsz * $bytes_per_val * $nbr_files_out; # no rank reduction
+    for (my $var_idx=1; $var_idx<=$var_nbr; $var_idx++) {
+	my $lmn_nbr = $var_idx * $var_sz;
+	$byt_in  = $lmn_nbr * $byt_per_val * $fl_nbr_in;
+	$byt_out = $lmn_nbr * $byt_per_val * $fl_nbr_out; # no rank reduction
 	
-	$disk_time_in = $bytes_in / $disk_bw_read;
-	$disk_time_out = $bytes_out / $disk_bw_write;
-	$disk_ttl_IO_time = $disk_time_in + $disk_time_out;
+	$dsk_tm_in = $byt_in / $dsk_bw_rd;
+	$dsk_tm_out = $byt_out / $dsk_bw_wrt;
+	$dsk_ttl_IO_tm = $dsk_tm_in + $dsk_tm_out;
 	
 	# match ZeM06 equations, unweighted
-	my $fp_cnts = $dsz;
-	my $fp_time = ($fp_cnts/$V_flops); # NB: $V_flops is dodgy
+	my $fp_cnts = $lmn_nbr;
+	my $fp_tm = ($fp_cnts/$V_flops); # NB: $V_flops is dodgy
 	
 	my $K_Ia = 1; # fudge for Ia
 	my $K_Mu = 1; # fudge for Mu (Mu may have to change relative to
-	my $Int_cnt =  $K_Ia *   6 * $dsz;
-	my $Mu_cnt =   $K_Mu *   3 * $bytes_per_val * $dsz;
+	my $Int_cnt =  $K_Ia *   6 * $lmn_nbr;
+	my $Mu_cnt =   $K_Mu *   3 * $byt_per_val * $lmn_nbr;
 	my $Ms_cnt = 0;
 	my $ncbo_int_op_cnts =  $Int_cnt + $Mu_cnt +  $Ms_cnt;
-	my $ncbo_int_time = $ncbo_int_op_cnts / $V_intops; # $V_intops is dodgy
+	my $ncbo_int_tm = $ncbo_int_op_cnts / $V_intops; # $V_intops is dodgy
 	
 	#         IO                 FP term                 Int term
-	$time = $disk_ttl_IO_time + $fp_time + $ncbo_int_time;
-	#print "$e $bytes_in $bytes_out  $disk_ttl_IO_time $fp_time  $ncbo_int_time $ncbo_Icyc_per_el\n";
-	printf     " %2d    %1.3e   %6.3f    %6.3f    %6.3f    %6.3f    %1.3e\n",  $e, $dsz, $fp_time, $ncbo_int_time, $disk_ttl_IO_time, $time, $ncbo_int_op_cnts;
-	printf GPD " %2d    %1.3e   %6.3f    %6.3f    %6.3f    %6.3f    %1.3e\n",  $e, $dsz, $fp_time, $ncbo_int_time, $disk_ttl_IO_time, $time, $ncbo_int_op_cnts;
+	$time = $dsk_ttl_IO_tm + $fp_tm + $ncbo_int_tm;
+	#print "$var_idx $byt_in $byt_out  $dsk_ttl_IO_tm $fp_tm  $ncbo_int_tm $ncbo_Icyc_per_el\n";
+	printf     " %2d    %1.3e   %6.3f    %6.3f    %6.3f    %6.3f    %1.3e\n",  $var_idx, $lmn_nbr, $fp_tm, $ncbo_int_tm, $dsk_ttl_IO_tm, $time, $ncbo_int_op_cnts;
+	printf GPD " %2d    %1.3e   %6.3f    %6.3f    %6.3f    %6.3f    %1.3e\n",  $var_idx, $lmn_nbr, $fp_tm, $ncbo_int_tm, $dsk_ttl_IO_tm, $time, $ncbo_int_op_cnts;
     }
 }
 
