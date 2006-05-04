61a157fcb825373a26c9a4621afa56f91c77ccce
Charlie Zender <zender@uci.edu>
2006-05-04 00:47:09 +0000

    *** empty log message ***

--- bm/clearcache.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-my $memsize =   80000000; # 200 els
-my @bigarray;
-print "Starting to load bigarray of size $memsize\n";
-for (my $r=0; $r<$memsize; $r++) {
-  $bigarray[$r] = $r;
-}
-print "Have we blown up yet?  Finished loading bigarray\n\n";
-print "File cache should be clear!\n";
