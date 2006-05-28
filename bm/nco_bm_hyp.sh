6de158ad5a94c1a5e6fa656cf2f7e67368489102
Charlie Zender <zender@uci.edu>
2006-05-28 04:06:08 +0000

    *** empty log message ***

--- bm/nco_bm_hyp.sh
+++ bm/nco_bm_hyp.sh
@@ -1,4 +1,4 @@
-# $Header: /data/zender/nco_20150216/nco/bm/nco_bm_hyp.sh,v 1.8 2006-05-27 21:48:33 zender Exp $
+# $Header: /data/zender/nco_20150216/nco/bm/nco_bm_hyp.sh,v 1.9 2006-05-28 04:06:03 zender Exp $
 
 # Purpose: Perform NCO benchmarks while subsetting 
 
@@ -21,39 +21,46 @@
 SCL_SUB_SET_FLG='1' # [flg] Use approach 1: Scale by sub-setting
 SCL_HYP_SLB_FLG='0' # [flg] Use approach 2: Scale by hyperslabbing
 
+WGT_FLG='0' # [flg] Perform weighted averages
+if [ ${WGT_FLG} = '1' ]; then
+    WGT_CMD="-w lat"
+else
+    WGT_CMD=''
+fi # !WGT_FLG
+
 CCH_CLR_FLG='1' # [flg] Clear cache before each command
 if [ ${CCH_CLR_FLG} = '1' ]; then
     CCH_CMD="${HOME}/nco/bm/cch_clr.pl"
 else
     CCH_CMD=''
-fi # endif
+fi # !CCH_CLR_FLG
 
 # Approach 1: Scale by sub-setting
 if [ ${SCL_SUB_SET_FLG} = '1' ]; then
 # 1/8
     ${CCH_CMD}; /usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	${MY_BIN_DIR}/ncwa -O --mdl -w lat -v 'd4_0[0-3],d3_0[0-7],d2_0[0-1],d1_0[0-0]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_01.out
+	${MY_BIN_DIR}/ncwa -O --mdl ${WGT_CMD} -v 'd4_0[0-3],d3_0[0-7],d2_0[0-1],d1_0[0-0]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_01.out
 # 2/8
     ${CCH_CMD}; /usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	${MY_BIN_DIR}/ncwa -O --mdl -w lat -v 'd4_0[0-7],d3_0[0-9],d3_1[0-5],d2_0[0-3],d1_0[0-1]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_02.out
+	${MY_BIN_DIR}/ncwa -O --mdl ${WGT_CMD} -v 'd4_0[0-7],d3_0[0-9],d3_1[0-5],d2_0[0-3],d1_0[0-1]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_02.out
 # 3/8
     ${CCH_CMD}; /usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	${MY_BIN_DIR}/ncwa -O --mdl -w lat -v 'd4_0[0-9],d4_1[0-1],d3_[0-1][0-9],d3_2[0-3],d2_0[0-5],d1_0[0-2]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_03.out
+	${MY_BIN_DIR}/ncwa -O --mdl ${WGT_CMD} -v 'd4_0[0-9],d4_1[0-1],d3_[0-1][0-9],d3_2[0-3],d2_0[0-5],d1_0[0-2]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_03.out
 # 4/8
     ${CCH_CMD}; /usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	${MY_BIN_DIR}/ncwa -O --mdl -w lat -v 'd4_0[0-9],d4_1[0-5],d3_[0-2][0-9],d3_3[0-1],d2_0[0-7],d1_0[0-3]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_04.out
+	${MY_BIN_DIR}/ncwa -O --mdl ${WGT_CMD} -v 'd4_0[0-9],d4_1[0-5],d3_[0-2][0-9],d3_3[0-1],d2_0[0-7],d1_0[0-3]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_04.out
 # 5/8
     ${CCH_CMD}; /usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	${MY_BIN_DIR}/ncwa -O --mdl -w lat -v 'd4_[0-1][0-9],d3_[0-3][0-9],d2_0[0-9],d1_0[0-4]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_05.out
+	${MY_BIN_DIR}/ncwa -O --mdl ${WGT_CMD} -v 'd4_[0-1][0-9],d3_[0-3][0-9],d2_0[0-9],d1_0[0-4]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_05.out
 # 6/8
     ${CCH_CMD}; /usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	${MY_BIN_DIR}/ncwa -O --mdl -w lat -v 'd4_[0-1][0-9],d4_2[0-3],d3_[0-3][0-9],d3_4[0-7],d2_0[0-9],d2_1[0-1],d1_0[0-5]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_06.out
+	${MY_BIN_DIR}/ncwa -O --mdl ${WGT_CMD} -v 'd4_[0-1][0-9],d4_2[0-3],d3_[0-3][0-9],d3_4[0-7],d2_0[0-9],d2_1[0-1],d1_0[0-5]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_06.out
 # 7/8
     ${CCH_CMD}; /usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	${MY_BIN_DIR}/ncwa -O --mdl -w lat -v 'd4_[0-1][0-9],d4_2[0-7],d3_[0-4][0-9],d3_5[0-5],d2_0[0-9],d2_1[0-3],d1_0[0-6]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_07.out
+	${MY_BIN_DIR}/ncwa -O --mdl ${WGT_CMD} -v 'd4_[0-1][0-9],d4_2[0-7],d3_[0-4][0-9],d3_5[0-5],d2_0[0-9],d2_1[0-3],d1_0[0-6]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_07.out
 # 8/8
     ${CCH_CMD}; /usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	${MY_BIN_DIR}/ncwa -O --mdl -w lat -v 'd4_[0-2][0-9],d4_3[0-1],d3_[0-5][0-9],d3_6[0-3],d2_0[0-9],d2_1[0-5],d1_0[0-7]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_08.out
+	${MY_BIN_DIR}/ncwa -O --mdl ${WGT_CMD} -v 'd4_[0-2][0-9],d4_3[0-1],d3_[0-5][0-9],d3_6[0-3],d2_0[0-9],d2_1[0-5],d1_0[0-7]' -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_08.out
 fi # !${SCL_SUB_SET_FLG}
 
 # Approach 2: Scale by hyperslabbing
@@ -66,6 +73,6 @@ if [ ${SCL_HYP_SLB_FLG} = '1' ]; then
 	fl_nbr_sng=`printf "%02d" ${fl_nbr}`
 	${CCH_CMD}
 	/usr/bin/time -a -o ~/nco/bm/ncwa.timing -f "%e wall %s system %U user " \
-	    ${MY_BIN_DIR}/ncwa -O -F --mdl -w lat -d time,1,${fl_nbr} -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_${fl_nbr_sng}.out
+	    ${MY_BIN_DIR}/ncwa -O -F --mdl ${WGT_CMD} -d time,1,${fl_nbr} -p ${DATA}/nco_bm ipcc_dly_T85.nc ${DATA}/nco_bm/foo.nc > ${DATA}/nco_bm/foo_${fl_nbr_sng}.out
     done # end loop over fl_nbr
 fi # !${SCL_HYP_SLB_FLG}
