e625cc68ecbcb11135189b680e35fede5032c14b
Henry Butowsky <henryb@hush.com>
2006-10-20 15:36:34 +0000

    first cut of double parsing

--- src/nco++/Ncap2.hh
+++ src/nco++/Ncap2.hh
@@ -1,4 +1,14 @@
 #ifndef NCAP2_HH
 #define NCAP2_HH
-typedef enum { ncap_var, ncap_att,ncap_hyper,ncap_undef  } ncap_type;
+
+typedef enum { ncap_var, 
+               ncap_att,
+               ncap_xpr_var_hyp,
+               ncap_xpr_att_hyp,
+               ncap_xpr_var_cst,
+	       ncap_xpr_null
+              } ncap_type;
+
+
+
 #endif // NCAP2_HH
