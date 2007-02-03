9377bc5cd34904749ff7e5a7e5390e7293be24ec
Daniel Wang <danielw@slac.stanford.edu>
2007-02-03 01:04:18 +0000

    Some more string changes; more friendly debug script; fixes to preserve SWAMP-lite sanity check testing; test script is less particular to my own installation; M chaining.ssdap M dap_nc_swamp M ssdebug M testchain.bash

--- src/ssdap/testchain.bash
+++ src/ssdap/testchain.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-bin/dap_nc_handler_hack -L -o dods -r /tmp -e superduperscript11 -c -u http://localhost:8000/cgi/nph_dods/foo_T42.nc -v DAP2/3.5.3 /home/wangd/opendap/aolserver4/servers/aoldap/pages/foo_T42.nc <chaining.ssdap
+./dap_nc_swamp -L -o dods -r /tmp -e superduperscript11 -c -u http://localhost:8000/cgi/nph_dods/foo_T42.nc -v DAP2/3.5.3 /var/www/html/foo_T42.nc <chaining.ssdap
 
