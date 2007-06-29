bd57f9436870d23e96acce9685a71a83e701fd4f
Charlie Zender <zender@uci.edu>
2007-06-29 15:01:21 +0000

    Finish deflation in ncap and ncap2

--- src/nco/ncap.c
+++ src/nco/ncap.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/ncap.c,v 1.212 2007-06-29 13:00:40 hmb Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/ncap.c,v 1.213 2007-06-29 15:00:42 zender Exp $ */
 
 /* ncap -- netCDF arithmetic processor */
 
@@ -123,8 +123,8 @@ main(int argc,char **argv)
   char *spt_arg[NCAP_SPT_NBR_MAX]; /* fxm: Arbitrary size, should be dynamic */
   char *spt_arg_cat=NULL; /* [sng] User-specified script */
 
-  const char * const CVS_Id="$Id: ncap.c,v 1.212 2007-06-29 13:00:40 hmb Exp $"; 
-  const char * const CVS_Revision="$Revision: 1.212 $";
+  const char * const CVS_Id="$Id: ncap.c,v 1.213 2007-06-29 15:00:42 zender Exp $"; 
+  const char * const CVS_Revision="$Revision: 1.213 $";
   const char * const opt_sht_lst="4ACcD:FfhL:l:n:Oo:p:Rrs:S:vx-:"; /* [sng] Single letter command line options */
 
 #if defined(__cplusplus) || defined(PGI_CC)
@@ -563,7 +563,7 @@ main(int argc,char **argv)
   prs_arg.var_lst=&var_ycc; /* [sct] Variables to be defined after 1st parse */
   prs_arg.nbr_var=&nbr_var_ycc; /* [nbr] Number of vars to be defined after 1st parse */
   prs_arg.nco_op_typ=nco_op_nil; /* [enm] Operation type */
-  prs_arg.dfl_lvl=dfl_lvl;
+  prs_arg.dfl_lvl=dfl_lvl; /* [enm] Deflate level */
   /* Timestamp end of metadata setup and disk layout */
   rcd+=nco_ddra((char *)NULL,(char *)NULL,&ddra_info);
   ddra_info.tmr_flg=nco_tmr_rgl;
@@ -743,7 +743,6 @@ main(int argc,char **argv)
     (void)nco_xrf_var(var[idx],var_out[idx]);
     (void)nco_xrf_dmn(var_out[idx]);
   } /* end loop over idx */
-  
 
   /* NB: ncap is not well-suited for nco_var_lst_dvd() */
   /* Divide variable lists into lists of fixed variables and variables to be processed */
--- src/nco/ncap.h
+++ src/nco/ncap.h
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/ncap.h,v 1.80 2007-06-29 13:01:24 hmb Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/ncap.h,v 1.81 2007-06-29 15:00:50 zender Exp $ */
 
 /* Purpose: netCDF arithmetic processor definitions and function prototypes for ncap.c, ncap_utl.c, ncap_lex.l, and ncap_yacc.y */
 
@@ -95,7 +95,7 @@ typedef struct{ /* prs_sct */
   var_sct ***var_lst; /* list of variables to be defined in O */
   int *nbr_var;        /* [nbr] of variables in above list */ 
   int nco_op_typ; /* [enm] Operation type */
-  int dfl_lvl;
+  int dfl_lvl; /* [enm] Deflate level */
 } prs_sct;
 
 /* Begin funtions in ncap_lex.l */
