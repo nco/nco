d482c4b42a7d0442f2bce68b5b2fafff5da66937
Henry Butowsky <henryb@hush.com>
2007-10-02 12:47:20 +0000

    Items in vector now sorted -all fuctions changed

--- src/nco++/NcapVarVector.cc
+++ src/nco++/NcapVarVector.cc
@@ -1,84 +1,92 @@
 #include "NcapVarVector.hh"
 
-NcapVar* NcapVarVector::find(const char*nm){
-  long idx;
-  long sz=t_vector.size();
-    for(idx=0; idx<sz;idx++)
-      if(!strcmp(nm, t_vector[idx]->getFll().c_str()))
-	return t_vector[idx];
-      return NULL;
+long NcapVarVector::size(){
+
+  return (long)std::vector<NcapVar*>::size();
+
 }
-
-
 
 
 NcapVar* NcapVarVector::find(std::string s_fnm){
-  NcapVar* Nvar;
-  Nvar=find(s_fnm.c_str());
-  return Nvar;
+long lret;
+  lret=findi(s_fnm);
+  if(lret >=0)
+    return (*this)[lret];
+
+  return NULL;
+
 }
 
+NcapVar* NcapVarVector::find(const char*nm){
+long lret;
+  lret=findi( std::string(nm));
+  if(lret >=0)
+    return (*this)[lret];
+
+  return NULL;
+}
 
 long NcapVarVector::findi(std::string s_fnm){
-  long idx;
-  long sz=t_vector.size();
-    for(idx=0; idx<sz;idx++)
-      if( s_fnm==t_vector[idx]->getFll())
-	return idx;
+    long lret;
+    NcapVar* Ntmp;
+    std::vector<NcapVar*>::iterator we;
+
+    if(size()==0)
       return -1;
-}
 
+    Ntmp=new NcapVar( (var_sct*)NULL ,s_fnm);
+    
+    we=std::lower_bound(begin(),end(),Ntmp,less_mag());
+   
 
-// remove top value
-NcapVar *NcapVarVector::pop(){
-    NcapVar *Ntmp; 
-
-    if(t_vector.size() > 0 ) {
-      Ntmp=t_vector.back();
-      t_vector.pop_back();
-      return Ntmp;
+    // Item already in vector
+    if( we !=end() && (*we)->getFll()==s_fnm ){
+      lret=0;
+      while( we-- != begin())  ++lret;  
+      
     }else
-      return NULL;
+     lret=-1;
+      
+    
+    // nb var_in also freed here  
+    delete Ntmp;     
+    
+    return lret;
+
 }
 
 
 
-
-// if Var exists overwrite, else add to vector
 void NcapVarVector::push_ow(NcapVar *Nvar){
-  long lret;
-  NcapVar *Ntmp;
-  lret=findi(Nvar->getFll());
-  if(lret >= 0 ) {
-    // delete current value
-    Ntmp= t_vector[lret];
-    // re-assign
-    t_vector[lret]=Nvar;
-    delete Ntmp;
-  }else   
-    (void)t_vector.push_back(Nvar);
-
+    push(Nvar);
 }
-
 
 
 // add to vector
 void NcapVarVector::push(NcapVar *Nvar){
-    (void)t_vector.push_back(Nvar);
+    std::vector<NcapVar*>::iterator we;
+
+
+    if(size()==0){
+      push_back(Nvar);
+	return;
+    }
+    
+    we=std::lower_bound(begin(),end(),Nvar,less_mag());
+   
+
+    // Item already in vector
+    if( we !=end() && (*we)->getFll()== Nvar->getFll()){
+      delete *we;
+      *we=Nvar;
+    // Insert Item
+    } else{
+      (void)std::vector<NcapVar*>::insert(we,Nvar);
+    }
 
 }
 
 
-
-// Delete an item
-void NcapVarVector::erase(long idx){
-  std::vector<NcapVar*>::iterator we=t_vector.begin();
-  
-  we+=idx;
-  delete t_vector[idx];
-  t_vector.erase(we);
-
-}
 
 
 // Delete an item
@@ -90,4 +98,72 @@ void NcapVarVector::erase(std::string s_fnm){
 
 }
 
+
+
+// Delete an item
+void NcapVarVector::erase(long idx){
+  std::vector<NcapVar*>::iterator we=begin();
+  we=begin();
+  we+=idx;
+  delete (*this)[idx];
+  std::vector<NcapVar*>::erase(we);
+
+}
+
+void NcapVarVector::sort(){
+  std::sort(begin(),end(),less_mag());
+}
+
+
+
+/*
+NcapVar* NcapVarVector::find(const char*nm){
+  long idx;
+  long sz=this->size();
+    for(idx=0; idx<sz;idx++)
+      if(!strcmp(nm, (*this)[idx]->getFll().c_str()))
+	return (*this)[idx];
+      return NULL;
+}
+
+*/
+
+/*
+NcapVar* NcapVarVector::find(std::string s_fnm){
+  NcapVar* Nvar;
+  Nvar=find(s_fnm.c_str());
+  return Nvar;
+}
+
+*/
+
+
+/*
+long NcapVarVector::findi(std::string s_fnm){
+  long idx;
+  long sz=this->size();
+    for(idx=0; idx<sz;idx++)
+      if( s_fnm==(*this)[idx]->getFll())
+	return idx;
+      return -1;
+}
+
+*/
+
+/*
+void NcapVarVector::push_ow(NcapVar *Nvar){
+  long lret;
+  NcapVar *Ntmp;
+  lret=findi(Nvar->getFll());
+  if(lret >= 0 ) {
+    // delete current value
+    Ntmp= (*this)[lret];
+    // re-assign
+    (*this)[lret]=Nvar;
+    delete Ntmp;
+  }else   
+    push_back(Nvar);
+
+}
+*/
 
