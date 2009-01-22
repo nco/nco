15f863eeb951769ede223d00e7903bf247d28b71
Charlie Zender <zender@uci.edu>
2009-01-22 23:07:06 +0000

    Preparing nco-3.9.6 release

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.226 2009-01-21 00:15:38 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.227 2009-01-22 23:02:44 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -720,7 +720,7 @@ const char * /* O [sng] Mnemonic that describes current NCO version */
 nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version */
-  return "Avant AGU\n";
+  return "Après AGU, SMA, et Bush!\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
