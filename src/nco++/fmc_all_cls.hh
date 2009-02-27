3b1606e77bfb9b35487efb71b6a031220c019ba4
Henry Butowsky <henryb@hush.com>
2009-02-27 12:53:16 +0000

    added srt_cls prototypes

--- src/nco++/fmc_all_cls.hh
+++ src/nco++/fmc_all_cls.hh
@@ -176,4 +176,19 @@ public:
 
 };
 
+
+
+//Sort Function /****************************************/
+class srt_cls: public vtl_cls {
+private:
+   enum {PSORT };
+   bool _flg_dbg;
+public:
+  srt_cls(bool flg_dbg);
+  var_sct *fnd(RefAST expr, RefAST fargs,fmc_cls &fmc_obj, ncoTree &walker);
+
+};
+
+
+
 #endif // FMC_ALL_CLS_HH
