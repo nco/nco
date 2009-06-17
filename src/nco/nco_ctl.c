183963bb34836954996295ff572993d8344a60e9
Charlie Zender <zender@uci.edu>
2009-06-17 03:47:34 +0000

    *** empty log message ***

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.246 2009-06-15 23:19:44 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.247 2009-06-17 03:47:34 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -736,7 +736,7 @@ const char * /* O [sng] Mnemonic that describes current NCO version */
 nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version */
-  return "Listen to Evolver, by John Legend. See I Love You, Man, with Paul Rudd. Read Bridge of Sighs, by Richard Russo.\n";
+  return "Listen to Evolver, by John Legend. Watch I Love You, Man, with Paul Rudd. Read Bridge of Sighs, by Richard Russo.\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
