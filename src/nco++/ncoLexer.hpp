35464586be72480437a7e62393c2f94a652ea0c9
Charlie Zender <zender@uci.edu>
2009-09-26 18:00:33 +0000

    Fix library flage for parallel HDF/netCDF4

--- src/nco++/ncoLexer.hpp
+++ src/nco++/ncoLexer.hpp
@@ -2,7 +2,7 @@
 #define INC_ncoLexer_hpp_
 
 #include <antlr/config.hpp>
-/* $ANTLR 2.7.7 (20090306): "ncoGrammer.g" -> "ncoLexer.hpp"$ */
+/* $ANTLR 2.7.7 (20060906): "ncoGrammer.g" -> "ncoLexer.hpp"$ */
 #include <antlr/CommonToken.hpp>
 #include <antlr/InputBuffer.hpp>
 #include <antlr/BitSet.hpp>
@@ -10,7 +10,7 @@
 #include <antlr/CharScanner.hpp>
 #line 1 "ncoGrammer.g"
 
-/* $Header: /data/zender/nco_20150216/nco/src/nco++/ncoLexer.hpp,v 1.81 2009-08-05 14:50:09 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco++/ncoLexer.hpp,v 1.82 2009-09-26 18:00:33 zender Exp $ */
 
 /* Purpose: ANTLR Grammar and support files for ncap2 */
 
