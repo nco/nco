cf2fe21a835bd418f0ab6acfc0977575abede0ba
Charlie Zender <zender@uci.edu>
2009-10-21 04:13:21 +0000

    *** empty log message ***

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.249 2009-07-26 18:55:50 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.250 2009-10-21 04:13:21 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -736,7 +736,7 @@ const char * /* O [sng] Mnemonic that describes current NCO version */
 nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version */
-  return "Trapezius muscle recovery\n";
+  return "Wii Dance\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
