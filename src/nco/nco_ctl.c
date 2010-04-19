e48ca8e346d7cad072f9a4fca7110e0ddbfacebb
Charlie Zender <zender@uci.edu>
2010-04-19 19:30:53 +0000

    *** empty log message ***

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.260 2010-04-18 17:59:53 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.261 2010-04-19 19:30:53 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -774,7 +774,7 @@ const char * /* O [sng] Mnemonic that describes current NCO version */
 nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version */
-  return "Percy Jackson\n";
+  return "Saw Ghost Town (hilarious), reading Percy Jackson Book II: The Sea of Monsters\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
