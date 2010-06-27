2665015aa94c871d0abdb45ede1bb16a1fe5c125
Charlie Zender <zender@uci.edu>
2010-06-27 20:44:13 +0000

    Preparing nco-4.0.3 release

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.264 2010-06-24 17:48:38 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.265 2010-06-27 20:44:13 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -774,7 +774,7 @@ const char * /* O [sng] Mnemonic that describes current NCO version */
 nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version */
-  return "Avatar: The time of great sorrow\n";
+  return "Why Zebras Don't Get Ulcers by Robert M. Sapolsky\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
