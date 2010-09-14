10e69cfc5401c0e66bdd20aeea19632cce654a68
Charlie Zender <zender@uci.edu>
2010-09-14 00:42:30 +0000

    return type of nco_cln_get_cal_typ, tm_typ, does not match lmt.lmt_cal

--- src/nco++/ncoLexer.hpp
+++ src/nco++/ncoLexer.hpp
@@ -2,7 +2,7 @@
 #define INC_ncoLexer_hpp_
 
 #include <antlr/config.hpp>
-/* $ANTLR 2.7.7 (20100319): "ncoGrammer.g" -> "ncoLexer.hpp"$ */
+/* $ANTLR 2.7.7 (20060906): "ncoGrammer.g" -> "ncoLexer.hpp"$ */
 #include <antlr/CommonToken.hpp>
 #include <antlr/InputBuffer.hpp>
 #include <antlr/BitSet.hpp>
@@ -10,7 +10,7 @@
 #include <antlr/CharScanner.hpp>
 #line 1 "ncoGrammer.g"
 
-/* $Header: /data/zender/nco_20150216/nco/src/nco++/ncoLexer.hpp,v 1.91 2010-07-28 23:28:04 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco++/ncoLexer.hpp,v 1.92 2010-09-14 00:39:35 zender Exp $ */
 
 /* Purpose: ANTLR Grammar and support files for ncap2 */
 
