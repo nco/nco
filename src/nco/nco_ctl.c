57f009c36cc15b7f172275ebeef9b5aa1ef1b4cf
Charlie Zender <zender@uci.edu>
2012-08-21 16:46:05 +0000

    Preparing nco-4.2.2 release

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.356 2012-08-20 22:01:11 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.357 2012-08-21 16:46:05 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -787,7 +787,7 @@ const char * /* O [sng] Mnemonic that describes current NCO version */
 nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version */
-  return "Just Janet.\n";
+  return "Farewell Lizzie.\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
