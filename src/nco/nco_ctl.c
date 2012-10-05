215118a318039d42b5c8eb50105b45b6185f644d
Charlie Zender <zender@uci.edu>
2012-10-05 14:57:18 +0000

    *** empty log message ***

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.359 2012-09-22 20:40:50 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.360 2012-10-05 14:57:18 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -787,7 +787,7 @@ const char * /* O [sng] Mnemonic that describes current NCO version */
 nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version */
-  return "CSUN\n";
+  return "Vegas, baby!\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
