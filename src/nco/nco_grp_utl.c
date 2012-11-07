f583ca10bcf4842a492af84c2f12437eb36c2359
Charlie Zender <zender@uci.edu>
2012-11-07 21:32:58 +0000

    Preparing nco-4.2.3 release

--- src/nco/nco_grp_utl.c
+++ src/nco/nco_grp_utl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_grp_utl.c,v 1.242 2012-11-07 21:10:49 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_grp_utl.c,v 1.243 2012-11-07 21:32:58 zender Exp $ */
 
 /* Purpose: Group utilities */
 
@@ -1128,7 +1128,7 @@ nco_grp_var_mk_trv                     /* [fnc] Create groups/write variables in
                 /* GPE might be already on the list, put it there only if not found */
                 for(int idx_gpe=0;idx_gpe<nbr_gpe_nm;idx_gpe++){
                   if(!strcmp(gpe_var_nm_fll,gpe_nm[idx_gpe].var_nm_fll)){
-                    (void)fprintf(stdout,"%s: ERROR nco_grp_var_mk_trv() reports variable %s already defined. HINT: Moving groups of flattening files can lead to ambiguous situations where a single object name (e.g., a variable name) must refer to multiple objects in the same output group. Instead of arbitrarily copying only one (e.g., the last) variable to the output file, NCO simply fails. User should re-try command after ensuring multiple objects of the same name will not be placed in the same group.\n",prg_nm_get(),gpe_var_nm_fll);
+                    (void)fprintf(stdout,"%s: ERROR nco_grp_var_mk_trv() reports variable %s already defined. HINT: Moving groups of flattening files can lead to over-determined situations where a single object name (e.g., a variable name) must refer to multiple objects in the same output group. The user's intent is ambiguous so instead of arbitrarily picking which (e.g., the last) variable of that name to place in the output file, NCO simply fails. User should re-try command after ensuring multiple objects of the same name will not be placed in the same group.\n",prg_nm_get(),gpe_var_nm_fll);
                     for(int idx=0;idx<nbr_gpe_nm;idx++)
                       gpe_nm[idx].var_nm_fll=(char *)nco_free(gpe_nm[idx].var_nm_fll);
                     nco_exit(EXIT_FAILURE);
