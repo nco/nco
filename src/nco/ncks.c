b5c439e9bf568f950e4448db489dd4554c7438ed
Pedro Vicente <pedro.vicente@space-research.org>
2012-12-05 22:46:16 +0000

    deprecate nco_var_lst_crd_ass_add (netCDF3 only) and use nco_var_lst_crd_ass_add_trv/nco_var_lst_crd_ass_add_cf_trv (netCDF3/4)

--- src/nco/ncks.c
+++ src/nco/ncks.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/ncks.c,v 1.496 2012-12-05 05:34:57 pvicente Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/ncks.c,v 1.497 2012-12-05 22:45:22 pvicente Exp $ */
 
 /* ncks -- netCDF Kitchen Sink */
 
@@ -150,8 +150,8 @@ main(int argc,char **argv)
   char *grp_out=NULL; /* [sng] Group name */
   char rth[]="/"; /* Group path */
 
-  const char * const CVS_Id="$Id: ncks.c,v 1.496 2012-12-05 05:34:57 pvicente Exp $"; 
-  const char * const CVS_Revision="$Revision: 1.496 $";
+  const char * const CVS_Id="$Id: ncks.c,v 1.497 2012-12-05 22:45:22 pvicente Exp $"; 
+  const char * const CVS_Revision="$Revision: 1.497 $";
   const char * const opt_sht_lst="346aABb:CcD:d:FG:g:HhL:l:MmOo:Pp:qQrRs:uv:X:xz-:";
   cnk_sct **cnk=NULL_CEWI;
 
@@ -725,16 +725,12 @@ main(int argc,char **argv)
  
   /* Extract coordinates associated with extracted variables */
   if(EXTRACT_ASSOCIATED_COORDINATES) {
-    if(HAS_SUBGRP){
-      xtr_lst=nco_var_lst_crd_ass_add_trv(in_id,xtr_lst,&xtr_nbr,trv_tbl);
-      if(CNV_CCM_CCSM_CF){
-        /* Add "coordinates" and "bounds" CF */
-        xtr_lst=nco_var_lst_crd_ass_add_cf_trv(in_id,"coordinates",xtr_lst,&xtr_nbr,trv_tbl);
-        xtr_lst=nco_var_lst_crd_ass_add_cf_trv(in_id,"bounds",xtr_lst,&xtr_nbr,trv_tbl);
-      } /* CNV_CCM_CCSM_CF */
-    }else{
-      xtr_lst=nco_var_lst_crd_ass_add(in_id,xtr_lst,&xtr_nbr,CNV_CCM_CCSM_CF);
-    } /* HAS_SUBGRP */
+    xtr_lst=nco_var_lst_crd_ass_add_trv(in_id,xtr_lst,&xtr_nbr,trv_tbl);
+    if(CNV_CCM_CCSM_CF){
+      /* Add "coordinates" and "bounds" CF */
+      xtr_lst=nco_var_lst_crd_ass_add_cf_trv(in_id,"coordinates",xtr_lst,&xtr_nbr,trv_tbl);
+      xtr_lst=nco_var_lst_crd_ass_add_cf_trv(in_id,"bounds",xtr_lst,&xtr_nbr,trv_tbl);
+    } /* CNV_CCM_CCSM_CF */
   } /* EXTRACT_ASSOCIATED_COORDINATES */
 
 
