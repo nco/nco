d70b6194a7bd72a79ed946638318e03b9d8592a9
Pedro Vicente <pedro.vicente@space-research.org>
2013-03-20 05:27:07 +0000

    Get number of variables, dimensions, and global attributes in file

--- src/nco/ncbo.c
+++ src/nco/ncbo.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/ncbo.c,v 1.212 2013-03-20 04:45:11 pvicente Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/ncbo.c,v 1.213 2013-03-20 05:27:07 pvicente Exp $ */
 
 /* ncbo -- netCDF binary operator */
 
@@ -130,8 +130,8 @@ main(int argc,char **argv)
 
   char trv_pth[]="/"; /* [sng] Root path of traversal tree */
 
-  const char * const CVS_Id="$Id: ncbo.c,v 1.212 2013-03-20 04:45:11 pvicente Exp $"; 
-  const char * const CVS_Revision="$Revision: 1.212 $";
+  const char * const CVS_Id="$Id: ncbo.c,v 1.213 2013-03-20 05:27:07 pvicente Exp $"; 
+  const char * const CVS_Revision="$Revision: 1.213 $";
   const char * const opt_sht_lst="346ACcD:d:Fg:hL:l:Oo:p:rRt:v:X:xzy:-:";
   
   cnk_sct **cnk=NULL_CEWI;
@@ -178,22 +178,31 @@ main(int argc,char **argv)
   int in_id_2;  
   int lmt_nbr=0; /* Option d. NB: lmt_nbr gets incremented */
   int md_open; /* [enm] Mode flag for nc_open() call */
-  int nbr_dmn_fl_1;
-  int nbr_dmn_fl_2;
+
+  int nbr_glb_att_1;  /* [nbr] Number of global attributes in file */
+  int nbr_glb_att_2;  /* [nbr] Number of global attributes in file */
+  int nbr_grp_att_1;  /* [nbr] Number of group attributes in file */
+  int nbr_grp_att_2;  /* [nbr] Number of group attributes in file */
+  int nbr_att_var_1;  /* [nbr] Number of variable attributes in file */
+  int nbr_att_var_2;  /* [nbr] Number of variable attributes in file */
+  int nbr_dmn_fl_1;   /* [nbr] Number of dimensions in file */
+  int nbr_dmn_fl_2;   /* [nbr] Number of dimensions in file */
+  int nbr_rec_fl_1;   /* [nbr] Number of record dimensions in file */
+  int nbr_rec_fl_2;   /* [nbr] Number of record dimensions in file */
+  int grp_dpt_fl_1;   /* [nbr] Maximum group depth (root = 0) */
+  int grp_dpt_fl_2;   /* [nbr] Maximum group depth (root = 0) */ 
+  int nbr_grp_fl_1;   /* [nbr] Number of groups in file */
+  int nbr_grp_fl_2;   /* [nbr] Number of groups in file */
+  int var_ntm_fl_1;   /* [nbr] Number of non-atomic variables in file */
+  int var_ntm_fl_2;   /* [nbr] Number of non-atomic variables in file */
+  int nbr_var_fl_1;   /* [nbr] Number of atomic-type variables in file */
+  int nbr_var_fl_2;   /* [nbr] Number of atomic-type variables in file */
+ 
   int nbr_dmn_xtr_1;
   int nbr_dmn_xtr_2;
-  int nbr_glb_att_1;
-  int nbr_glb_att_2;
-  int nbr_att_var_1;
-  int nbr_att_var_2;
-  int nbr_grp_fl_1;
-  int nbr_grp_fl_2;
-  int nbr_rec_fl_1;
-  int nbr_rec_fl_2;
+
   int nbr_var_fix_1; /* nbr_var_fix_1 gets incremented */
   int nbr_var_fix_2; /* nbr_var_fix_2 gets incremented */
-  int nbr_var_fl_1;
-  int nbr_var_fl_2;
   int nbr_var_prc_1; /* nbr_var_prc_1 gets incremented */
   int nbr_var_prc_2; /* nbr_var_prc_2 gets incremented */
   int xtr_nbr_1=0; /* xtr_nbr_1 won't otherwise be set for -c with no -v */
@@ -546,11 +555,11 @@ main(int argc,char **argv)
      } /* endif aux */
   } /* endif aux_nbr */
 
-  
+ 
+  /* Get number of variables, dimensions, and global attributes in file, file format */
+  (void)trv_tbl_inq(&nbr_glb_att_1,&nbr_grp_att_1,&nbr_att_var_1,&nbr_dmn_fl_1,&nbr_rec_fl_1,&grp_dpt_fl_1,&nbr_grp_fl_1,&var_ntm_fl_1,&nbr_var_fl_1,trv_tbl_1);
+  (void)trv_tbl_inq(&nbr_glb_att_2,&nbr_grp_att_2,&nbr_att_var_2,&nbr_dmn_fl_2,&nbr_rec_fl_2,&grp_dpt_fl_2,&nbr_grp_fl_2,&var_ntm_fl_2,&nbr_var_fl_2,trv_tbl_2);
 
-  /* Get number of variables and dimensions in file */
-  (void)nco_inq(in_id_1,&nbr_dmn_fl_1,&nbr_var_fl_1,(int *)NULL,(int *)NULL);
-  (void)nco_inq(in_id_2,&nbr_dmn_fl_2,&nbr_var_fl_2,(int *)NULL,(int *)NULL);
   (void)nco_inq_format(in_id_1,&fl_in_fmt_1);
   (void)nco_inq_format(in_id_2,&fl_in_fmt_2);
 
