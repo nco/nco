e220252e8eb76df7ea830a03fc361932a8d60d6f
Pedro Vicente <pedro.vicente@space-research.org>
2013-05-20 21:45:39 +0000

    add debug message

--- src/nco/ncpdq.c
+++ src/nco/ncpdq.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/ncpdq.c,v 1.217 2013-05-17 22:33:23 pvicente Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/ncpdq.c,v 1.218 2013-05-20 21:45:39 pvicente Exp $ */
 
 /* ncpdq -- netCDF pack, re-dimension, query */
 
@@ -126,8 +126,8 @@ main(int argc,char **argv)
   char scl_fct_sng[]="scale_factor"; /* [sng] Unidata standard string for scale factor */
   char trv_pth[]="/"; /* [sng] Root path of traversal tree */
 
-  const char * const CVS_Id="$Id: ncpdq.c,v 1.217 2013-05-17 22:33:23 pvicente Exp $"; 
-  const char * const CVS_Revision="$Revision: 1.217 $";
+  const char * const CVS_Id="$Id: ncpdq.c,v 1.218 2013-05-20 21:45:39 pvicente Exp $"; 
+  const char * const CVS_Revision="$Revision: 1.218 $";
   const char * const opt_sht_lst="346Aa:CcD:d:Fg:G:hL:l:M:Oo:P:p:Rrt:v:UxZ-:";
 
   cnk_sct **cnk=NULL_CEWI;
@@ -721,6 +721,14 @@ main(int argc,char **argv)
 
     (void)nco_xrf_var(var[idx_var],var_out[idx_var]);
     (void)nco_xrf_dmn(var_out[idx_var]);
+
+    if(dbg_lvl_get() >= nco_dbg_dev){
+      (void)fprintf(stdout,"%s: DEBUG variable <%s>: ",prg_nm_get(),var_out[idx_var]->nm_fll);
+      for(int idx_dmn=0;idx_dmn<var_out[idx_var]->nbr_dim;idx_dmn++){
+        (void)fprintf(stdout,"[%d]%s ",idx_dmn,var_out[idx_var]->dim[idx_dmn]->nm);
+      }
+      (void)fprintf(stdout,"\n");
+    } /* endif dbg */       
   
    
   }
