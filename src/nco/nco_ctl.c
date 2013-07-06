a6346aaee789cfa64e21e3f662c26944a32f0be0
Charlie Zender <zender@uci.edu>
2013-07-06 00:39:16 +0000

    Preparing nco-4.3.3 release

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.394 2013-07-05 22:20:29 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.395 2013-07-06 00:39:16 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -794,7 +794,7 @@ const char * /* O [sng] Mnemonic that describes current NCO version */
 nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version */
-  return "Post-Socorro\n";
+  return "Best of OC: Fourth of July on Balboa Peninsula\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
