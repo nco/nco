2bb15528d82a7108e3ec0d4f191c54c6de153e28
Pedro Vicente <pedro.vicente@space-research.org>
2013-08-27 22:33:24 +0000

    comment a free call in strcasecmp that is giving heap corruption errors in MSVC

--- src/nco/nco_sng_utl.c
+++ src/nco/nco_sng_utl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_sng_utl.c,v 1.56 2013-08-27 22:07:56 pvicente Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_sng_utl.c,v 1.57 2013-08-27 22:33:24 pvicente Exp $ */
 
 /* Purpose: String utilities */
 
@@ -25,18 +25,24 @@ strcasecmp /* [fnc] Lexicographical case-insensitive string comparison */
     chr_2=tolower(*sng_2_c++);
     if(chr_1 < chr_2){
       /* Use free() not nco_free() to keep this more like a generic (non-NCO) library function */
+#ifndef _MSC_VER
       free(sng_1_c);
       free(sng_2_c);
+#endif
       return -1;
     }
     if(chr_1 > chr_2){
+#ifndef _MSC_VER
       free(sng_1_c);
       free(sng_2_c);
+#endif
       return 1;
     }
     if(chr_1 == 0){
+#ifndef _MSC_VER
       free(sng_1_c);
       free(sng_2_c);
+#endif
       return 0;
     }
   } /* end while */
