97aee50781ae8cf2de533ba7e320da7a2f51ab69
Charlie Zender <zender@uci.edu>
2013-08-28 20:11:30 +0000

    remove dangerous $ in nco.texi

--- src/nco++/ncoTree.hpp
+++ src/nco++/ncoTree.hpp
@@ -3,12 +3,12 @@
 
 #include <antlr/config.hpp>
 #include "ncoParserTokenTypes.hpp"
-/* $ANTLR 2.7.7 (2006-11-01): "ncoGrammer.g" -> "ncoTree.hpp"$ */
+/* $ANTLR 2.7.7 (20120518): "ncoGrammer.g" -> "ncoTree.hpp"$ */
 #include <antlr/TreeParser.hpp>
 
 #line 1 "ncoGrammer.g"
 
-/* $Header: /data/zender/nco_20150216/nco/src/nco++/ncoTree.hpp,v 1.129 2013-08-27 21:07:42 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco++/ncoTree.hpp,v 1.130 2013-08-28 20:11:30 zender Exp $ */
 
 /* Purpose: ANTLR Grammar and support files for ncap2 */
 
