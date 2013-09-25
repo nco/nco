feb86f0e9e0dfdd9ad96f70f08420983857bd5f8
Wenshan Wang <wenshanw@uci.edu>
2013-09-25 17:40:51 +0000

    fix ncpqd record dimension

--- doc/xmp/ann_avg_grp.sh
+++ doc/xmp/ann_avg_grp.sh
@@ -26,7 +26,7 @@ flt='nc'     # File Type
 var='sn'      # Variable
 lbl='all-mdl_all-xpt_all-nsm'     # Label
 
-# Greenland
+## Greenland
 ncwa -O -w gw -d lat,60.0,75.0 -d lon,300.0,340.0 -a lat,lon \
   ${drc}${var}_${rlm}_${lbl}_${tms}.${flt} \
   ${drc}${var}_${rlm}_${lbl}_${tms}_gld.${flt}
@@ -34,7 +34,7 @@ ncwa -O -w gw -d lat,60.0,75.0 -d lon,300.0,340.0 -a lat,lon \
 ncwa -O -w gw -d lat,30.0,40.0 -d lon,80.0,100.0 -a lat,lon \
   ${drc}${var}_${rlm}_${lbl}_${tms}.${flt} \
   ${drc}${var}_${rlm}_${lbl}_${tms}_tbt.${flt}
-# time is no longer the record dimension in the outputs
+# Time is no longer the record dimension in the outputs
 ncks -O --mk_rec_dmn time \
   ${drc}${var}_${rlm}_${lbl}_${tms}_tbt.${flt} \
   ${drc}${var}_${rlm}_${lbl}_${tms}_tbt.${flt}
@@ -42,7 +42,7 @@ ncks -O --mk_rec_dmn time \
   ${drc}${var}_${rlm}_${lbl}_${tms}_gld.${flt} \
   ${drc}${var}_${rlm}_${lbl}_${tms}_gld.${flt}
 
-# Concatenate 2 regions together
+# Aggregate 2 regions together
 ncecat -O -u rgn ${drc}${var}_${rlm}_${lbl}_${tms}_???.${flt} \
   ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2.${flt}
 # Change dimensions order
@@ -50,27 +50,19 @@ ncpdq -O -a time,rgn ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2.${flt} \
   ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2.${flt}
 
 # Remove temporary files (optional)
-#rm ${drc}${var}_${rlm}_${lbl}_${tms}_???.${flt}
+rm ${drc}${var}_${rlm}_${lbl}_${tms}_???.${flt}
 
 #Annual average
-#ncra -O --mro -d time,,,12,12 ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2.${flt} \
+ncra -O --mro -d time,,,12,12 ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2.${flt} \
   ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2_ann.${flt}
-ncra -O --mro -d time,,,12,12 ${drc}${var}_${rlm}_${lbl}_${tms}_gld.${flt} \
-  ${drc}${var}_${rlm}_${lbl}_${tms}_gld_ann.${flt}
 
 # Anomaly
 #------------------------------------------------------------
 # Long-term average
-#ncwa -O -a time ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2_ann.${flt} \
-#  ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2_clm.${flt}
-ncwa -O -a time ${drc}${var}_${rlm}_${lbl}_${tms}_gld_ann.${flt} \
-  ${drc}${var}_${rlm}_${lbl}_${tms}_gld_clm.${flt}
+ncwa -O -a time ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2_ann.${flt} \
+  ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2_clm.${flt}
 # Subtract
-ncbo -O --op_typ=- ${drc}${var}_${rlm}_${lbl}_${tms}_gld_ann.${flt} \
-  ${drc}${var}_${rlm}_${lbl}_${tms}_gld_clm.${flt} \
-  ${drc}${var}_${rlm}_${lbl}_${tms}_gld_anm.${flt}
+ncbo -O --op_typ=- ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2_ann.${flt} \
+  ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2_clm.${flt} \
+  ${drc}${var}_${rlm}_${lbl}_${tms}_rgn2_anm.${flt}
 #------------------------------------------------------------
-
-# Standard Deviation
-# Needs to confirm
-#ncea -O -y rmssdn ${drc}${var}_${rlm}_${lbl}_${tms}_gld_anm.${flt} ${drc}${var}_${rlm}_${lbl}_${tms}_gld_anm-sdv.${flt}
