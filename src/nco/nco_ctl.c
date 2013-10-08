3610a75ee53fe40fce116a8d7b32ba749339ae35
Charlie Zender <zender@uci.edu>
2013-10-08 04:22:35 +0000

    Preparing nco-4.3.7 release

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.422 2013-10-04 00:17:10 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.423 2013-10-08 04:22:35 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -795,7 +795,7 @@ nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version
      Always Include terminal \n so mnemonic does not dangle */
-  return "Django Unchained\n";
+  return "Gravity\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
