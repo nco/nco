dc1b813c859f0663ff5bbe9ab85218857d6efc7a
Charlie Zender <zender@uci.edu>
2013-12-20 06:56:43 +0000

    ncdismember handles HDF4 files (sort of), work on excluding ancestor out-of-scope coordinates

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.443 2013-12-17 21:34:29 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/nco_ctl.c,v 1.444 2013-12-20 06:56:43 zender Exp $ */
 
 /* Purpose: Program flow control functions */
 
@@ -799,7 +799,7 @@ nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version
      Always Include terminal \n so mnemonic does not dangle */
-  return "DOE better late than never\n";
+  return "She did it again!\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
