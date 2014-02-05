a72013c8aa188a4a0ede2a30150a3623cc960c4c
Charlie Zender <zender@uci.edu>
2014-02-05 20:35:29 +0000

    Improve documentation build rules in ./configure

--- bm/NCO_rgr.pm
+++ bm/NCO_rgr.pm
@@ -1,6 +1,6 @@
 package NCO_rgr;
 
-# $Header: /data/zender/nco_20150216/nco/bm/NCO_rgr.pm,v 1.432 2014-02-05 20:07:09 pvicente Exp $
+# $Header: /data/zender/nco_20150216/nco/bm/NCO_rgr.pm,v 1.433 2014-02-05 20:35:29 zender Exp $
 
 # Purpose: All REGRESSION tests for NCO operators
 # BENCHMARKS are coded in "NCO_benchmarks.pm"
@@ -4785,15 +4785,14 @@ print "\n";
     NCO_bm::tst_run(\@tst_cmd);
     $#tst_cmd=0; # Reset array		
 	
-
 #NEW 4.4.0	
 #ncwa #55
 #ncwa -O -4  -a time --cnk_dmn lon,4 -v byt_3D_rec in.nc out.nc
     
-	$dsc_sng="Chunking -a time --cnk_dmn lon,4 -v byt_3D_rec";
+    $dsc_sng="Chunking -a time --cnk_dmn lon,4 -v byt_3D_rec";
     $tst_cmd[0]="ncwa $omp_flg -O -4 $nco_D_flg -a time --cnk_dmn lon,4 -v byt_3D_rec $in_pth_arg in.nc %tmp_fl_00%";
     $tst_cmd[1]="ncks -m %tmp_fl_00% | grep 'byt_3D_rec dimension 1'";
-	$tst_cmd[2]="byt_3D_rec dimension 1: lon, size = 4 NC_FLOAT, chunksize = 4 (Coordinate is lon)";
+    $tst_cmd[2]="byt_3D_rec dimension 1: lon, size = 4 NC_FLOAT, chunksize = 4 (Coordinate is lon)";
     $tst_cmd[3]="SS_OK";
     NCO_bm::tst_run(\@tst_cmd);
     $#tst_cmd=0; # Reset array		
@@ -4802,10 +4801,10 @@ print "\n";
 #ncwa #56
 #ncwa -O -4 -a /time --cnk_dmn /time,1 -v time  in.nc out.nc
     
-	$dsc_sng="Chunking -a /time --cnk_dmn /time,1 -v time";
+    $dsc_sng="Chunking -a /time --cnk_dmn /time,1 -v time";
     $tst_cmd[0]="ncwa $omp_flg -O -4 $nco_D_flg -a /time --cnk_dmn /time,1 -v time $in_pth_arg in.nc %tmp_fl_00%";
     $tst_cmd[1]="ncks -m %tmp_fl_00% | grep 'time: type'";
-	$tst_cmd[2]="time: type NC_DOUBLE, 0 dimensions, 5 attributes, chunked? no, compressed? no, packed? no";
+    $tst_cmd[2]="time: type NC_DOUBLE, 0 dimensions, 5 attributes, chunked? no, compressed? no, packed? no";
     $tst_cmd[3]="SS_OK";
     NCO_bm::tst_run(\@tst_cmd);
     $#tst_cmd=0; # Reset array		
@@ -4818,7 +4817,7 @@ print "\n";
     $dsc_sng="Chunking with -rdd -a lon,lat -v four_dmn_rec_var --cnk_dmn lat,2 --cnk_dmn lon,4 ";
     $tst_cmd[0]="ncwa $omp_flg -O -C -4 $nco_D_flg --rdd -a lon,lat -v four_dmn_rec_var --cnk_dmn lat,2 --cnk_dmn lon,4 $in_pth_arg in.nc %tmp_fl_00%";
     $tst_cmd[1]="ncks --hdn --cdl -v four_dmn_rec_var %tmp_fl_00% | grep 'four_dmn_rec_var:_ChunkSizes'";
-	$tst_cmd[2]="      four_dmn_rec_var:_ChunkSizes = 1, 1, 3, 1 ;";
+    $tst_cmd[2]="      four_dmn_rec_var:_ChunkSizes = 1, 1, 3, 1 ;";
     $tst_cmd[3]="SS_OK";
     NCO_bm::tst_run(\@tst_cmd);
     $#tst_cmd=0; # Reset array	
@@ -4831,16 +4830,12 @@ print "\n";
     $dsc_sng="Cell methods -a time -v time";
     $tst_cmd[0]="ncwa $omp_flg $nco_D_flg -O -a time -v time -C $in_pth_arg in.nc %tmp_fl_00%";
     $tst_cmd[1]="ncks -m %tmp_fl_00%";
-	$tst_cmd[2]="time attribute 4: cell_methods, size = 11 NC_CHAR, value = time: mean";
+    $tst_cmd[2]="time attribute 4: cell_methods, size = 11 NC_CHAR, value = time: mean";
     $tst_cmd[3]="SS_OK";
     NCO_bm::tst_run(\@tst_cmd);
     $#tst_cmd=0; # Reset array		
-
 	
 	} #### Group tests	
-	
-
-	
     
 ####################
 #### ncrename tests #### OK!
