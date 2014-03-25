3cc34d14de38656a42d8b0884db8b0e68536bb9e
Henry Butowsky <henryb@hush.com>
2014-03-25 14:19:36 +0000

    mini tidy up

--- src/nco++/ncap2.cc
+++ src/nco++/ncap2.cc
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco++/ncap2.cc,v 1.188 2014-02-14 05:22:17 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco++/ncap2.cc,v 1.189 2014-03-25 14:19:36 hmb Exp $ */
 
 /* ncap2 -- netCDF arithmetic processor */
 
@@ -145,8 +145,8 @@ main(int argc,char **argv)
   char *spt_arg[NCAP_SPT_NBR_MAX]; /* fxm: Arbitrary size, should be dynamic */
   char *spt_arg_cat=NULL_CEWI; /* [sng] User-specified script */
   
-  const char * const CVS_Id="$Id: ncap2.cc,v 1.188 2014-02-14 05:22:17 zender Exp $"; 
-  const char * const CVS_Revision="$Revision: 1.188 $";
+  const char * const CVS_Id="$Id: ncap2.cc,v 1.189 2014-03-25 14:19:36 hmb Exp $"; 
+  const char * const CVS_Revision="$Revision: 1.189 $";
   const char * const att_nm_tmp="eulaVlliF_"; /* For netCDF4 name hack */
   const char * const opt_sht_lst="3467ACcD:FfhL:l:n:Oo:p:Rrs:S:t:vx-:"; /* [sng] Single letter command line options */
   
@@ -522,8 +522,7 @@ main(int argc,char **argv)
   // Co-ordinates functions
   cod_cls cod_obj(true); 
 
-  // misc functions 
-  misc_cls misc_obj(true);
+
 
   // Populate vector
   (void)pop_fmc_vtr(fmc_vtr,&cnv_obj);
@@ -540,7 +539,6 @@ main(int argc,char **argv)
   (void)pop_fmc_vtr(fmc_vtr,&arr_obj);
   (void)pop_fmc_vtr(fmc_vtr,&bil_obj);
   (void)pop_fmc_vtr(fmc_vtr,&cod_obj);
-  (void)pop_fmc_vtr(fmc_vtr,&misc_obj);
 
 #ifdef ENABLE_GSL
 #ifdef ENABLE_NCO_GSL
