6a9c9279098878e0f7320171d76d513ddccb52c8
Charlie Zender <zender@uci.edu>
2015-01-20 19:59:25 +0000

    Fix lsd_big in CDL

--- src/nco/ncpdq.c
+++ src/nco/ncpdq.c
@@ -1,4 +1,4 @@
-/* $Header: /data/zender/nco_20150216/nco/src/nco/ncpdq.c,v 1.413 2015-01-17 00:49:35 zender Exp $ */
+/* $Header: /data/zender/nco_20150216/nco/src/nco/ncpdq.c,v 1.414 2015-01-20 19:59:25 zender Exp $ */
 
 /* ncpdq -- netCDF pack, re-dimension, query */
 
@@ -137,8 +137,8 @@ main(int argc,char **argv)
   char scl_fct_sng[]="scale_factor"; /* [sng] Unidata standard string for scale factor */
   char trv_pth[]="/"; /* [sng] Root path of traversal tree */
 
-  const char * const CVS_Id="$Id: ncpdq.c,v 1.413 2015-01-17 00:49:35 zender Exp $"; 
-  const char * const CVS_Revision="$Revision: 1.413 $";
+  const char * const CVS_Id="$Id: ncpdq.c,v 1.414 2015-01-20 19:59:25 zender Exp $"; 
+  const char * const CVS_Revision="$Revision: 1.414 $";
   const char * const opt_sht_lst="3467Aa:CcD:d:Fg:G:hL:l:M:Oo:P:p:Rrt:v:UxZ-:";
 
   cnk_sct cnk; /* [sct] Chunking structure */
@@ -941,8 +941,8 @@ main(int argc,char **argv)
       if(nco_pck_map_sng) nco_pck_map_sng=(char *)nco_free(nco_pck_map_sng);
       if(nco_pck_plc != nco_pck_plc_upk){
         /* No need for loop over var_prc variables to free attribute values
-        Variable structures and attribute edit lists share same attribute values
-        Free them only once, and do it in nco_var_free() */
+	   Variable structures and attribute edit lists share same attribute values
+	   Free them only once, and do it in nco_var_free() */
         aed_lst_add_fst=(aed_sct *)nco_free(aed_lst_add_fst);
         aed_lst_scl_fct=(aed_sct *)nco_free(aed_lst_scl_fct);
       } /* nco_pck_plc == nco_pck_plc_upk */
