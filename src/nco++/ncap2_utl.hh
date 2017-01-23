d0a35a70c35f2592484f990735a943bfdbaa580c
hmb1 <henryb@hush.com>
2017-01-23 15:08:10 +0000

    removed functions that are now in ncap2_att.cc/hh

--- src/nco++/ncap2_utl.hh
+++ src/nco++/ncap2_utl.hh
@@ -57,7 +57,7 @@
 #include "sdo_utl.hh"
 #include "VarOpNew.hh" 
 #include "prs_cls.hh"
-
+#include "ncap2_att.hh"
 
 /* Don't know what Charlies done to the bools */
 /* Temporary fix for now !!*/
@@ -105,6 +105,11 @@ nco_shp_chk(
 var_sct* var1, 
 var_sct* var2); 
 
+var_sct *      /* initialize var to defaults & undefined to true */
+ncap_var_udf
+(const char *var_nm);
+ 
+
 
 nco_bool 
 ncap_var_is_op_doable( 
@@ -125,87 +130,6 @@ var_sct* var2);
 
 
 /***************************************************************************************/
-
-/***************  att functions ********************************************************/
-
-nco_bool        /* Reurns True if var has attribute style name */
-ncap_var_is_att( 
-var_sct *var);
-
-std::string ncap_att2var
-( prs_cls *prs_arg,   
-  std::string att_nm);
-
-
-var_sct*               /* O [sct] variable containing attribute */
-ncap_att_get	       /*   [fnc] Grab an attribute from input file */	
-(int var_id,           /*   I  var id        */ 
- const char *var_nm,   /*   I [sng] var name */
- const char *att_nm,   /*   I [sng] att name */
- int location,         /*   I [flg] 1 - att from INPUT file  2 - att from OUTPUT file */
- prs_cls *prs_arg);    /* I/O vectors of atts & vars & file names  */
-
-
-var_sct *                /* O [sct] variable containing attribute */
-ncap_att_init(           /*   [fnc] Grab an attribute from input file */
-std::string va_nm,       /* I [sng] att name of form var_nm&att_nm */ 
-prs_cls *prs_arg);       /* I/O vectors of atts & vars & file names  */
-
-
-
-int
-ncap_att_gnrl
-(const std::string s_dst,
- const std::string s_src,
- int location,         /*   I [flg] 1 - att from INPUT file  2 - att from OUTPUT file */
- prs_cls  *prs_arg);
-
-
-
-nco_bool                /* O [flg] true if var has been stretched */
-ncap_att_stretch    /* stretch a single valued attribute from 1 to sz */
-(var_sct* var,      /* I/O [sct] variable */       
- long nw_sz);       /* I [nbr] new var size */
-
-var_sct *      /* initialize var to defaults & undefined to true */
-ncap_var_udf
-(const char *var_nm);
- 
-
-int 
-ncap_att_cpy_sct
-(var_sct *var1,
- var_sct *var2,
- prs_cls  *prs_arg);
-
-int             
-ncap_att_cpy
-(const std::string s_dst,
- const std::string s_src,
- prs_cls  *prs_arg);
-
-void 
-ncap_att_prn   
-(var_sct *var, 
- char *const att_in_sng);
-
-char *            /* [0] sng - malloced inside function */
-ncap_att_sprn     /* [fnc] Print a single attribute*/
-(var_sct *var,   /* I Variable containing att */
- char *const att_in_sng); /* user defined format string */
-
-
-int                 /* number appended */ 
-ncap_att_str        /* extract string(s) from a NC_CHAR or NC_STRING type attribute */
-(var_sct *var_att,  /* I [sct] input attribute */
- std::vector<std::string> &str_vtr);
-
-char *            /* new malloc'ed string */ 
-ncap_att_char    /* extract string from a NC_CHAR or first NC_STRING */
-(var_sct *var_att);
-
-
-/******************************************************************************************/
 
 
 /*************** custom math functions *********************************************************/
@@ -286,80 +210,6 @@ nco_att_lst_mk
 
 /******************************************************************************************/
 
-/*********************** make scalar vars/atts ********************************************/
-
-// ncap_sclr_var_mk() overloads
-/* Create a scalar variable of type, if bfill then malloc ptr_unn */
-var_sct*
-ncap_sclr_var_mk(
-const std::string var_nm,
-nc_type type,
-//bool bfll=false); fxm csz
-bool bfll);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-float val_float);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-double val_double);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_int val_int);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_short val_short);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_char val_char);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_byte val_byte);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_ubyte val_ubyte);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_ushort val_ushort);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_uint val_uint);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_int64 val_int64);
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_uint64 val_uint64);
-
-
-
-var_sct *
-ncap_sclr_var_mk(
-const std::string var_nm,
-nco_string val_string);
-
-/******************************************************************************************/
 
 /************************ casting functions ***********************************************/
 
