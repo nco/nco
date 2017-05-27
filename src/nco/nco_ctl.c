742cef0aa7eaf26bbbc3b1f1d3ad6b384f73d281
Charlie Zender <zender@uci.edu>
2017-05-26 18:03:30 -0700

    lazy commit

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -844,7 +844,7 @@ nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version
      Always include terminal \n so mnemonic does not dangle */
-  return "Shots and power tools\n";
+  return "Orange Inn\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
