b9e8348ecceabca988fc12898bbb5be655a139f5
pedro-vicente <pedro.vicente@space-research.org>
2017-09-03 15:33:32 -0400

    add GSL detection to CMakeLists.txt

--- cmake/build.bat
+++ cmake/build.bat
@@ -10,5 +10,8 @@ cmake .. ^
 -DANTLR_LIBRARY:FILE=I:\antlr-2.7.7\lib\cpp\debug\antlr.lib ^
 -DUDUNITS2_INCLUDE:PATH=I:\udunits2\lib ^
 -DUDUNITS2_LIBRARY:FILE=I:\udunits2\build\lib\Debug\udunits2.lib ^
--DEXPAT_LIBRARY:FILE=I:\expat\expat\win32\bin\Debug\libexpatMT.lib
+-DEXPAT_LIBRARY:FILE=I:\expat\expat\win32\bin\Debug\libexpatMT.lib ^
+-DGSL_INCLUDE:PATH=I:\gsl-1.8\src ^
+-DGSL_LIBRARY:FILE=I:\gsl-1.8\src\gsl\1.8\gsl-1.8\VC8\libgsl\Debug-StaticLib\libgsl_d.lib
+
 
