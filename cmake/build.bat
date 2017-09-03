56d7476a430315687e6f58332885c361b3ac81bc
pedro-vicente <pedro.vicente@space-research.org>
2017-09-03 15:03:38 -0400

    Add UDUNITS2 detection to CMake script

---
+++ cmake/build.bat
@@ -0,0 +1,14 @@
+cmake .. ^
+-DNETCDF_INCLUDE:PATH=I:\grafic_project\netcdf\include ^
+-DNETCDF_LIBRARY:FILE=I:\grafic_project\Debug\netcdf.lib ^
+-DHDF5_LIBRARY:FILE=I:\grafic_project\Debug\hdf5.1.8.5.patch1.lib ^
+-DHDF5_HL_LIBRARY:FILE=I:\grafic_project\Debug\hdf5_hl.1.8.5.patch1.lib ^
+-DZLIB_LIBRARY:FILE=I:\grafic_project\Debug\zlib.lib ^
+-DSZIP_LIBRARY:FILE=I:\grafic_project\Debug\szlib.lib ^
+-DCURL_LIBRARY:FILE=I:\grafic_project\Debug\libcurld.lib ^
+-DANTLR_INCLUDE:PATH=I:\antlr-2.7.7\lib\cpp ^
+-DANTLR_LIBRARY:FILE=I:\antlr-2.7.7\lib\cpp\debug\antlr.lib ^
+-DUDUNITS2_INCLUDE:PATH=I:\udunits2\lib ^
+-DUDUNITS2_LIBRARY:FILE=I:\udunits2\build\lib\Debug\udunits2.lib ^
+-DEXPAT_LIBRARY:FILE=I:\expat\expat\win32\bin\Debug\libexpatMT.lib
+
