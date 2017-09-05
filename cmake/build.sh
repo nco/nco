2e4362821a20da6dd26ed00f434b7d504c3ad616
Pedro Vicente <pvicente@glace>
2017-09-05 12:59:10 -0700

    add install target and example cmake script

--- cmake/build.sh
+++ cmake/build.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 cmake .. \
+-DCMAKE_INSTALL_PREFIX=/home/pvicente/nco_install \
 -DANTLR_INCLUDE:PATH=/home/pvicente/install/antlr-2.7.7/include \
 -DANTLR_LIBRARY:FILE=/home/pvicente/install/antlr-2.7.7/lib/libantlr.a \
 -DGSL_INCLUDE:PATH=/home/pvicente/install/gsl-1.16/include \
