d3d9adc6ca48b4a8aba263f6a5ceb0d36b9c7313
Pedro Vicente <pvicente@glace>
2017-09-08 07:52:51 -0700

    add mention of GSL CBLAS library

--- cmake/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-cmake .. \
--DCMAKE_INSTALL_PREFIX=/home/pvicente/nco_install \
--DANTLR_INCLUDE:PATH=/home/pvicente/install/antlr-2.7.7/include \
--DANTLR_LIBRARY:FILE=/home/pvicente/install/antlr-2.7.7/lib/libantlr.a \
--DGSL_INCLUDE:PATH=/home/pvicente/install/gsl-1.16/include \
--DGSL_LIBRARY:FILE=/home/pvicente/install/gsl-1.16/lib/libgsl.a
