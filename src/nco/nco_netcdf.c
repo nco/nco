0e92af5e637f002dc5a0c4d69d36dbc16edf874b
Charlie Zender <zender@uci.edu>
2017-11-15 19:43:29 -0800

    long/size_t workaround for var1 routines (second try)

--- src/nco/nco_netcdf.c
+++ src/nco/nco_netcdf.c
@@ -1909,22 +1909,24 @@ nco_get_var1(const int nc_id,const int var_id,const long * const srt,void * cons
   /* Purpose: Wrapper for nc_get_var1_*() */
   const char fnc_nm[]="nco_get_var1()";
   int rcd=NC_NOERR;
-  size_t srt_sz_t; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
-  srt_sz_t=*srt;
+  int dmn_nbr; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
+  size_t srt_sz_t[NC_MAX_VAR_DIMS];
+  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
+  for(int dmn_idx=0;dmn_idx<dmn_nbr;dmn_idx++) srt_sz_t[dmn_idx]=srt[dmn_idx];
   switch(var_typ){
-  case NC_FLOAT: rcd=nc_get_var1_float(nc_id,var_id,&srt_sz_t,(float *)vp); break;
-  case NC_DOUBLE: rcd=nc_get_var1_double(nc_id,var_id,&srt_sz_t,(double *)vp); break;
-  case NC_INT: rcd=NCO_GET_VAR1_INT(nc_id,var_id,&srt_sz_t,(nco_int *)vp); break;
-  case NC_SHORT: rcd=nc_get_var1_short(nc_id,var_id,&srt_sz_t,(nco_short *)vp); break;
-  case NC_CHAR: rcd=NCO_GET_VAR1_CHAR(nc_id,var_id,&srt_sz_t,(nco_char *)vp); break;
-  case NC_BYTE: rcd=NCO_GET_VAR1_BYTE(nc_id,var_id,&srt_sz_t,(nco_byte *)vp); break;
+  case NC_FLOAT: rcd=nc_get_var1_float(nc_id,var_id,srt_sz_t,(float *)vp); break;
+  case NC_DOUBLE: rcd=nc_get_var1_double(nc_id,var_id,srt_sz_t,(double *)vp); break;
+  case NC_INT: rcd=NCO_GET_VAR1_INT(nc_id,var_id,srt_sz_t,(nco_int *)vp); break;
+  case NC_SHORT: rcd=nc_get_var1_short(nc_id,var_id,srt_sz_t,(nco_short *)vp); break;
+  case NC_CHAR: rcd=NCO_GET_VAR1_CHAR(nc_id,var_id,srt_sz_t,(nco_char *)vp); break;
+  case NC_BYTE: rcd=NCO_GET_VAR1_BYTE(nc_id,var_id,srt_sz_t,(nco_byte *)vp); break;
 #ifdef ENABLE_NETCDF4
-  case NC_UBYTE: rcd=NCO_GET_VAR1_UBYTE(nc_id,var_id,&srt_sz_t,(nco_ubyte *)vp); break;
-  case NC_USHORT: rcd=NCO_GET_VAR1_USHORT(nc_id,var_id,&srt_sz_t,(nco_ushort *)vp); break;
-  case NC_UINT: rcd=NCO_GET_VAR1_UINT(nc_id,var_id,&srt_sz_t,(nco_uint *)vp); break;
-  case NC_INT64: rcd=NCO_GET_VAR1_INT64(nc_id,var_id,&srt_sz_t,(nco_int64 *)vp); break;
-  case NC_UINT64: rcd=NCO_GET_VAR1_UINT64(nc_id,var_id,&srt_sz_t,(nco_uint64 *)vp); break;
-  case NC_STRING: rcd=NCO_GET_VAR1_STRING(nc_id,var_id,&srt_sz_t,(nco_string *)vp); break;
+  case NC_UBYTE: rcd=NCO_GET_VAR1_UBYTE(nc_id,var_id,srt_sz_t,(nco_ubyte *)vp); break;
+  case NC_USHORT: rcd=NCO_GET_VAR1_USHORT(nc_id,var_id,srt_sz_t,(nco_ushort *)vp); break;
+  case NC_UINT: rcd=NCO_GET_VAR1_UINT(nc_id,var_id,srt_sz_t,(nco_uint *)vp); break;
+  case NC_INT64: rcd=NCO_GET_VAR1_INT64(nc_id,var_id,srt_sz_t,(nco_int64 *)vp); break;
+  case NC_UINT64: rcd=NCO_GET_VAR1_UINT64(nc_id,var_id,srt_sz_t,(nco_uint64 *)vp); break;
+  case NC_STRING: rcd=NCO_GET_VAR1_STRING(nc_id,var_id,srt_sz_t,(nco_string *)vp); break;
 #endif /* !ENABLE_NETCDF4 */
   default: nco_dfl_case_nc_type_err(); break;
   } /* end switch */
@@ -1943,21 +1945,23 @@ nco_put_var1(const int nc_id,const int var_id,const long * const srt,const void 
   /* Purpose: Wrapper for nc_put_var1_*() */
   const char fnc_nm[]="nco_put_var1()";
   int rcd=NC_NOERR;
-  size_t srt_sz_t; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
-  srt_sz_t=*srt;
+  int dmn_nbr; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
+  size_t srt_sz_t[NC_MAX_VAR_DIMS];
+  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
+  for(int dmn_idx=0;dmn_idx<dmn_nbr;dmn_idx++) srt_sz_t[dmn_idx]=srt[dmn_idx];
   switch(type){
-  case NC_FLOAT: rcd=nc_put_var1_float(nc_id,var_id,&srt_sz_t,(const float *)vp); break;
-  case NC_DOUBLE: rcd=nc_put_var1_double(nc_id,var_id,&srt_sz_t,(const double *)vp); break;
-  case NC_INT: rcd=NCO_PUT_VAR1_INT(nc_id,var_id,&srt_sz_t,(const nco_int *)vp); break;
-  case NC_SHORT: rcd=nc_put_var1_short(nc_id,var_id,&srt_sz_t,(const short *)vp); break;
-  case NC_CHAR: rcd=NCO_PUT_VAR1_CHAR(nc_id,var_id,&srt_sz_t,(const nco_char *)vp); break;
-  case NC_BYTE: rcd=NCO_PUT_VAR1_BYTE(nc_id,var_id,&srt_sz_t,(const nco_byte *)vp); break;
+  case NC_FLOAT: rcd=nc_put_var1_float(nc_id,var_id,srt_sz_t,(const float *)vp); break;
+  case NC_DOUBLE: rcd=nc_put_var1_double(nc_id,var_id,srt_sz_t,(const double *)vp); break;
+  case NC_INT: rcd=NCO_PUT_VAR1_INT(nc_id,var_id,srt_sz_t,(const nco_int *)vp); break;
+  case NC_SHORT: rcd=nc_put_var1_short(nc_id,var_id,srt_sz_t,(const short *)vp); break;
+  case NC_CHAR: rcd=NCO_PUT_VAR1_CHAR(nc_id,var_id,srt_sz_t,(const nco_char *)vp); break;
+  case NC_BYTE: rcd=NCO_PUT_VAR1_BYTE(nc_id,var_id,srt_sz_t,(const nco_byte *)vp); break;
 #ifdef ENABLE_NETCDF4
-  case NC_UBYTE: rcd=NCO_PUT_VAR1_UBYTE(nc_id,var_id,&srt_sz_t,(const nco_ubyte *)vp); break;
-  case NC_USHORT: rcd=NCO_PUT_VAR1_USHORT(nc_id,var_id,&srt_sz_t,(const nco_ushort *)vp); break;
-  case NC_UINT: rcd=NCO_PUT_VAR1_UINT(nc_id,var_id,&srt_sz_t,(const nco_uint *)vp); break;
-  case NC_INT64: rcd=NCO_PUT_VAR1_INT64(nc_id,var_id,&srt_sz_t,(const nco_int64 *)vp); break;
-  case NC_UINT64: rcd=NCO_PUT_VAR1_UINT64(nc_id,var_id,&srt_sz_t,(const nco_uint64 *)vp); break;
+  case NC_UBYTE: rcd=NCO_PUT_VAR1_UBYTE(nc_id,var_id,srt_sz_t,(const nco_ubyte *)vp); break;
+  case NC_USHORT: rcd=NCO_PUT_VAR1_USHORT(nc_id,var_id,srt_sz_t,(const nco_ushort *)vp); break;
+  case NC_UINT: rcd=NCO_PUT_VAR1_UINT(nc_id,var_id,srt_sz_t,(const nco_uint *)vp); break;
+  case NC_INT64: rcd=NCO_PUT_VAR1_INT64(nc_id,var_id,srt_sz_t,(const nco_int64 *)vp); break;
+  case NC_UINT64: rcd=NCO_PUT_VAR1_UINT64(nc_id,var_id,srt_sz_t,(const nco_uint64 *)vp); break;
     /* Next line produces GCC warning:
        attention : passing argument 4 of ‘nc_put_var1_string’ from incompatible pointer type 
        I think this warning occurs because this routine receives input vp 
@@ -1970,13 +1974,13 @@ nco_put_var1(const int nc_id,const int var_id,const long * const srt,const void 
        Choose solution that allows trouble-free g++ compilation */
     /* Next line produces gcc and g++ warning:
        attention : le transtypage annule des qualificateurs du type pointeur ciblé */
-  case NC_STRING: rcd=NCO_PUT_VAR1_STRING(nc_id,var_id,&srt_sz_t,(const char **)vp); break;
+  case NC_STRING: rcd=NCO_PUT_VAR1_STRING(nc_id,var_id,srt_sz_t,(const char **)vp); break;
       /* Next line produces g++ warning:
        erreur: invalid conversion from ‘char* const* const’ to ‘const char** */
-    /* case NC_STRING: rcd=NCO_PUT_VAR1_STRING(nc_id,var_id,&srt_sz_t,(const nco_string * const)vp); break;*/
+    /* case NC_STRING: rcd=NCO_PUT_VAR1_STRING(nc_id,var_id,srt_sz_t,(const nco_string * const)vp); break;*/
     /* Next line produces g++ warning:
         erreur: invalid conversion from ‘char* const*’ to ‘const char** */
-    /*  case NC_STRING: rcd=NCO_PUT_VAR1_STRING(nc_id,var_id,&srt_sz_t,(const nco_string *)vp); break;*/
+    /*  case NC_STRING: rcd=NCO_PUT_VAR1_STRING(nc_id,var_id,srt_sz_t,(const nco_string *)vp); break;*/
 #endif /* !ENABLE_NETCDF4 */
   default: nco_dfl_case_nc_type_err(); break;
   } /* end switch */
@@ -1996,9 +2000,9 @@ nco_get_vara(const int nc_id,const int var_id,const long * const srt,const long 
   const char fnc_nm[]="nco_get_vara()";
   int rcd=NC_NOERR;
   int dmn_nbr; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
-  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   size_t cnt_sz_t[NC_MAX_VAR_DIMS];
   size_t srt_sz_t[NC_MAX_VAR_DIMS];
+  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   for(int dmn_idx=0;dmn_idx<dmn_nbr;dmn_idx++){
     cnt_sz_t[dmn_idx]=cnt[dmn_idx];
     srt_sz_t[dmn_idx]=srt[dmn_idx];
@@ -2055,9 +2059,9 @@ nco_put_vara(const int nc_id,const int var_id,const long * const srt,const long 
   const char fnc_nm[]="nco_put_vara()";
   int rcd=NC_NOERR;
   int dmn_nbr; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
-  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   size_t cnt_sz_t[NC_MAX_VAR_DIMS];
   size_t srt_sz_t[NC_MAX_VAR_DIMS];
+  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   for(int dmn_idx=0;dmn_idx<dmn_nbr;dmn_idx++){
     cnt_sz_t[dmn_idx]=cnt[dmn_idx];
     srt_sz_t[dmn_idx]=srt[dmn_idx];
@@ -2096,10 +2100,10 @@ nco_get_vars(const int nc_id,const int var_id,const long * const srt,const long 
   const char fnc_nm[]="nco_get_vars()";
   int rcd=NC_NOERR;
   int dmn_nbr; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
-  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   ptrdiff_t srd_pd_t[NC_MAX_VAR_DIMS];
   size_t cnt_sz_t[NC_MAX_VAR_DIMS];
   size_t srt_sz_t[NC_MAX_VAR_DIMS];
+  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   for(int dmn_idx=0;dmn_idx<dmn_nbr;dmn_idx++){
     cnt_sz_t[dmn_idx]=cnt[dmn_idx];
     srd_pd_t[dmn_idx]=srd[dmn_idx];
@@ -2138,10 +2142,10 @@ nco_put_vars(const int nc_id,const int var_id,const long * const srt,const long 
   const char fnc_nm[]="nco_put_vars()";
   int rcd=NC_NOERR;
   int dmn_nbr; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
-  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   ptrdiff_t srd_pd_t[NC_MAX_VAR_DIMS];
   size_t cnt_sz_t[NC_MAX_VAR_DIMS];
   size_t srt_sz_t[NC_MAX_VAR_DIMS];
+  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   for(int dmn_idx=0;dmn_idx<dmn_nbr;dmn_idx++){
     cnt_sz_t[dmn_idx]=cnt[dmn_idx];
     srd_pd_t[dmn_idx]=srd[dmn_idx];
@@ -2180,11 +2184,11 @@ nco_get_varm(const int nc_id,const int var_id,const long * const srt,const long 
   /* Purpose: Wrapper for nc_get_varm_*() */
   int rcd=NC_NOERR;
   int dmn_nbr; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
-  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   ptrdiff_t map_pd_t[NC_MAX_VAR_DIMS];
   ptrdiff_t srd_pd_t[NC_MAX_VAR_DIMS];
   size_t cnt_sz_t[NC_MAX_VAR_DIMS];
   size_t srt_sz_t[NC_MAX_VAR_DIMS];
+  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   for(int dmn_idx=0;dmn_idx<dmn_nbr;dmn_idx++){
     cnt_sz_t[dmn_idx]=cnt[dmn_idx];
     map_pd_t[dmn_idx]=map[dmn_idx];
@@ -2218,11 +2222,11 @@ nco_put_varm(const int nc_id,const int var_id,const long * const srt,const long 
   /* Purpose: Wrapper for nc_put_varm_*() */
   int rcd=NC_NOERR;
   int dmn_nbr; /* 20171115: WIN64 workaround: sizeof(long) = 4 != 8 = sizeof(size_t) */
-  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   ptrdiff_t map_pd_t[NC_MAX_VAR_DIMS];
   ptrdiff_t srd_pd_t[NC_MAX_VAR_DIMS];
   size_t cnt_sz_t[NC_MAX_VAR_DIMS];
   size_t srt_sz_t[NC_MAX_VAR_DIMS];
+  rcd=nc_inq_varndims(nc_id,var_id,&dmn_nbr);
   for(int dmn_idx=0;dmn_idx<dmn_nbr;dmn_idx++){
     cnt_sz_t[dmn_idx]=cnt[dmn_idx];
     map_pd_t[dmn_idx]=map[dmn_idx];
