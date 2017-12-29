aad9150bc3334e7c0628e7d755125435056c3d5c
pedro-vicente <pedro.vicente@space-research.org>
2017-12-29 13:55:22 -0500

    add a batch file to git clone NCO dependencies add a batch file to cmake build using nmake (requires fix to broken netcdf cmake script, INCLUDE_DIRECTORIES(${HDF5_HL_INCLUDE_DIR})

---
+++ cmake/clone.bat
@@ -0,0 +1,8 @@
+
+git clone https://github.com/Unidata/netcdf-c
+pushd netcdf-c
+git checkout tags/v4.5.0
+popd
+git clone https://github.com/curl/curl
+git clone https://github.com/madler/zlib
+git clone https://github.com/live-clones/hdf5
