8ba65e027706211778a9ff702bb8ccf4d4a0b584
Henry Butowsky <hbutowsk@skyglow.ess.uci.edu>
2018-02-05 04:11:51 -0800

    big tidy up

--- bm/cch_clr.sh
+++ bm/cch_clr.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
 # Flush and clear the disk caches.
 
-if [[ "$OSTYPE" = "linux-gnu" ]]; then
-  sync
-  sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
-elif [[ "$OSTYPE" = "macos" ]]; then
-  purge
-fi    
+if [ -z "$OSTYPE" ]; then
+    echo "$0: unknown OS type env-var OSTYPE not set"
+    exit 1;
+fi
+
+case "$OSTYPE" in
     
+  darwin*)
+    purge
+    ;;
+  
+  linux*)
+    echo "$0: about to clear caches for linux"  
+    sync
+    sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+    ;;
+	 
+  bsd*)
+    ;;
+  
+  # windoze
+  msys*)
+    ;;
+
+  #default do nothing
+  *)
+    ;;   
+
+esac
+
+exit 0
