69f9085c755665d6af613df1c44a722c8e20f959
Charlie Zender <zender@uci.edu>
2018-02-22 13:55:54 -0800

    Do not call sanitizer on system memory

--- src/nco/nco_fl_utl.c
+++ src/nco/nco_fl_utl.c
@@ -383,7 +383,6 @@ nco_fl_lst_mk /* [fnc] Create file list from command line positional arguments *
   /* Might there be problems with any specified files? */
   for(idx=arg_crr;idx<argc;idx++){
     if((int)strlen(argv[idx]) >= fl_nm_sz_wrn) (void)fprintf(stderr,"%s: WARNING filename %s is very long (%ld characters) and may not be portable to older operating systems\n",nco_prg_nm_get(),argv[idx],(long int)strlen(argv[idx]));
-    (void)nco_sng_sntz(argv[idx]);
   } /* end loop over idx */
 
   /* All operators except multi-file operators must have at least one positional argument */
@@ -539,7 +538,7 @@ nco_fl_lst_mk /* [fnc] Create file list from command line positional arguments *
     nco_exit(EXIT_FAILURE);
   } /* end if */
 
-  /* Sanitize input list from stdin or from positional arguments */
+  /* Sanitize input list from stdin and from positional arguments */
   for(int fl_idx=0;fl_idx<*fl_nbr;fl_idx++) (void)nco_sng_sntz(fl_lst_in[fl_idx]);
 
   /* Assign output file from positional argument */
