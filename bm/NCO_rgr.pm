983f358e07b7f70d385811b16e9644f4c59f1311
Charlie Zender <zender@uci.edu>
2018-07-27 08:12:26 -0700

    lazy commit

--- bm/NCO_rgr.pm
+++ bm/NCO_rgr.pm
@@ -3087,12 +3087,10 @@ if($RUN_NETCDF4_TESTS_VERSION_GE_431){
 	NCO_bm::tst_run(\@tst_cmd);
 	$#tst_cmd=0; # Reset array
     } # !XML
-
-
     
 # ncks #127    
     $dsc_sng="ncks test auxiliary coords WITH standard_name" ;
-    $tst_cmd[0]="ncks -O $fl_fmt $nco_D_flg -X 128.0,134.0,-16.0,-9.0  $in_pth_arg rgr.nc %tmp_fl_01%"; 
+    $tst_cmd[0]="ncks -O $fl_fmt $nco_D_flg -X 128.0,134.0,-16.0,-9.0 $in_pth_arg rgr.nc %tmp_fl_01%"; 
     $tst_cmd[1]="ncap2 -O $fl_fmt $nco_D_flg -S '../data/chk-aux-subset.nco' %tmp_fl_01% %tmp_fl_02%";
     $tst_cmd[2]="ncks -O -C -H --trd -v nbr_err -s '%d' %tmp_fl_02%";
     $tst_cmd[3]="0";
@@ -3101,10 +3099,10 @@ if($RUN_NETCDF4_TESTS_VERSION_GE_431){
     $#tst_cmd=0; # Reset array
 
 # ncks #128    
-    $dsc_sng="ncks test auxiliary coords WITH-OUT standard_name";
-    $tst_cmd[0]="ncatted -O $fl_fmt $nco_D_flg -a standard_name,,d,i,1 $in_pth_arg   rgr.nc   %tmp_fl_00%";
-    $tst_cmd[1]="ncks -O $fl_fmt $nco_D_flg -X 128.0,134.0,-16.0,-9.0  %tmp_fl_00%  %tmp_fl_01%";
-    # $tst_cmd[1]="ncks -O $fl_fmt $nco_D_flg -X 128.0,134.0,-16.0,-9.0  '../data/row.nc'  %tmp_fl_01%"; 
+    $dsc_sng="ncks test auxiliary coords WITHOUT standard_name";
+    $tst_cmd[0]="ncatted -O $fl_fmt $nco_D_flg -a standard_name,,d,i,1 $in_pth_arg rgr.nc %tmp_fl_00%";
+    $tst_cmd[1]="ncks -O $fl_fmt $nco_D_flg -X 128.0,134.0,-16.0,-9.0 %tmp_fl_00% %tmp_fl_01%";
+    # $tst_cmd[1]="ncks -O $fl_fmt $nco_D_flg -X 128.0,134.0,-16.0,-9.0 '../data/row.nc' %tmp_fl_01%"; 
     $tst_cmd[2]="ncap2 -v -O $fl_fmt $nco_D_flg -S '../data/chk-aux-subset.nco' %tmp_fl_01% %tmp_fl_02%";
     $tst_cmd[3]="ncks -O -C -H --trd -v nbr_err -s '%d' %tmp_fl_02%";
     $tst_cmd[4]="0";
