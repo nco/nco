2fbdedc1e44208d15afa64d866c8a9fcfa644de5
Charlie Zender <zender@uci.edu>
2018-08-30 14:15:28 -0700

    Version 4.7.7-alpha02: Kya Party of 8

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -838,7 +838,7 @@ nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version
      Always include terminal \n so mnemonic does not dangle */
-  return "Mnemonic: Dana TopBoat\n";
+  return "Mnemonic: K'ya Party of 8\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
