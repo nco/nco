5fec16e0319a8ed5a40471e0b7e72291604a5c49
Charlie Zender <zender@uci.edu>
2018-09-26 16:45:51 -0700

    lazy commit

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -842,7 +842,7 @@ nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version
      Always include terminal \n so mnemonic does not dangle */
-  return "Mnemonic: Imua MVB\n";
+  return "Mnemonic: Ohana Fest\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
