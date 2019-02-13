59a933e926bd59b05b3dfe2d00211dbbcc1163f1
hmb1 <henryb@hush.com>
2019-02-13 14:00:41 +0000

    first cut

---
+++ src/nco/nco_ply_lst.c
@@ -0,0 +1,14 @@
+
+#include "nco_ply_lst.h"
+
+
+int
+hello(
+char * msg)
+{
+
+  (void)fprintf(stdout, "hello %s\n", msg);
+
+  return 1;
+
+}
---
+++ src/nco/nco_ply_lst.h
@@ -0,0 +1,37 @@
+#ifndef NCO_PLY_LST_H /* Contents have not yet been inserted in current source file */
+#define NCO_PLY_LST_H
+
+
+#include        <stdlib.h>
+#include        <stdio.h>
+#include        <math.h>
+
+/* Personal headers */
+#include "nco.h" /* netCDF Operator (NCO) definitions */
+#include "nco_mmr.h"     /* Memory management */
+#include "nco_omp.h"     /* OpenMP utilities */
+#include "nco_rgr.h"     /* Regridding */
+#include "nco_sld.h"     /* Swath-Like Data */
+#include "nco_sng_utl.h" /* String utilities */
+#include "nco_ply.h"    /* poly sct stuff */
+
+
+
+#ifdef __cplusplus
+/* Use C-bindings so C++-compiled and C-compiled libraries are compatible */
+extern "C" {
+#endif /* !__cplusplus */
+
+int
+hello(char *msg);
+
+
+
+
+
+#ifdef __cplusplus
+} /* end extern "C" */
+#endif /* !__cplusplus */
+
+#endif /* NCO_PLY_LST_H */
+
