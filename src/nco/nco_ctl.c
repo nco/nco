7345bf248cdb46f14fe0661664fdad595b62d71a
Charlie Zender <zender@uci.edu>
2019-04-24 19:42:23 -0700

    Version 4.8.0-alpha11: Jazz Fest

--- src/nco/nco_ctl.c
+++ src/nco/nco_ctl.c
@@ -845,7 +845,7 @@ nco_nmn_get(void) /* [fnc] Return mnemonic that describes current NCO version */
 { 
   /* Purpose: Return mnemonic describing current NCO version
      Always include terminal \n so mnemonic does not dangle */
-  return "Mnemonic: UTRAX Trainee 249\n";
+  return "Mnemonic: Jazz Fest\n";
 } /* end nco_nmn_get() */
 
 char * /* O [sng] nm_in stripped of any path (i.e., program name stub) */ 
