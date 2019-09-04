23c776ba504f4c3e30489252065ac37f3bcb7e37
hmb1 <henryb@hush.com>
2019-09-04 14:42:37 +0100

    minor fixes

--- src/nco/nco_map.c
+++ src/nco/nco_map.c
@@ -1006,7 +1006,7 @@ nco_msh_mk /* [fnc] Compute overlap mesh and weights */
     } /* !pl_cnt_in */
 
     if(nco_dbg_lvl_get() >= nco_dbg_dev)
-      fprintf(stderr, "%s: INFO: num input polygons=%d, num output polygons=%d num overlap polygons=%d\n", nco_prg_nm_get(),pl_cnt_in, pl_cnt_out, pl_cnt_vrl);
+      fprintf(stderr, "%s: INFO: num input polygons=%d, num output polygons=%d num overlap polygons=%d\n", nco_prg_nm_get(),grd_sz_in, grd_sz_out , pl_cnt_vrl);
 
   }
 
