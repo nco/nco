45d5c8503177e832a7b3bf107e2c77ef4ce201d4
hbdch <sahilkatrekar@gmail.com>
2020-07-06 20:36:26 -0700

    Commit to sahil fork

--- src/nco/nco_rgr.c
+++ src/nco/nco_rgr.c
@@ -4774,19 +4774,19 @@ nco_rgr_wgt /* [fnc] Regrid with external weights */
 	  if(!has_mss_val){
 	    if(lvl_nbr == 1){
 	      /* Weight single-level fields without missing values */
-	      //#pragma omp target data map(to:col_src_adr[0:lnk_nbr],row_dst_adr[0:lnk_nbr],var_val_dbl_in[0:var_sz_in],wgt_raw[0:lnk_nbr]) map(tofrom:var_val_dbl_out[0:var_sz_out])
-	      //#pragma omp target teams distribute parallel for simd
+	      #pragma omp target data map(to:col_src_adr[0:lnk_nbr],row_dst_adr[0:lnk_nbr],var_val_dbl_in[0:var_sz_in],wgt_raw[0:lnk_nbr]) map(tofrom:var_val_dbl_out[0:var_sz_out])
+	      #pragma omp target teams distribute parallel for simd schedule(static,1)
 	      for(lnk_idx=0;lnk_idx<lnk_nbr;lnk_idx++)
 		var_val_dbl_out[row_dst_adr[lnk_idx]]+=var_val_dbl_in[col_src_adr[lnk_idx]]*wgt_raw[lnk_idx];
 	    }else{
 	      val_in_fst=0L;
 	      val_out_fst=0L;
 	      /* Weight multi-level fields without missing values */
-	      //#pragma omp target data map(to:col_src_adr[0:lnk_nbr],row_dst_adr[0:lnk_nbr],var_val_dbl_in[0:var_sz_in],wgt_raw[0:lnk_nbr]) map(tofrom:var_val_dbl_out[0:var_sz_out])
-	      //#pragma omp parallel for reduction(+:val_in_fst,val_out_fst)
+	      #pragma omp target data map(to:col_src_adr[0:lnk_nbr],row_dst_adr[0:lnk_nbr],var_val_dbl_in[0:var_sz_in],wgt_raw[0:lnk_nbr]) map(tofrom:var_val_dbl_out[0:var_sz_out])
+	      #pragma omp parallel for reduction(+:val_in_fst,val_out_fst)
 	      for(lvl_idx=0;lvl_idx<lvl_nbr;lvl_idx++){
 		//if(nco_dbg_lvl_get() >= nco_dbg_crr) (void)fprintf(fp_stdout,"%s lvl_idx = %d val_in_fst = %li, val_out_fst = %li\n",trv.nm,lvl_idx,val_in_fst,val_out_fst);
-		//#pragma omp target teams distribute parallel for simd
+		#pragma omp target teams distribute parallel for simd schedule(static,1)
 		for(lnk_idx=0;lnk_idx<lnk_nbr;lnk_idx++)
 		  var_val_dbl_out[row_dst_adr[lnk_idx]+val_out_fst]+=var_val_dbl_in[col_src_adr[lnk_idx]+val_in_fst]*wgt_raw[lnk_idx];
 		val_in_fst+=grd_sz_in;
