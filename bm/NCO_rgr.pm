043c8bbe471f109bb066a9933710cac4c7b2d4e3
Charlie Zender <zender@uci.edu>
2020-08-05 11:43:28 -0700

    Parallelize diurnal climo generation from multiple input files. Turn-off AMWG links by default (enable old behavior with --amwg_links). Default December mode is sdd (enable old behavior with --dec_md=scd).

--- bm/NCO_rgr.pm
+++ bm/NCO_rgr.pm
@@ -157,11 +157,11 @@ print "\n";
     $HOME=$ENV{'HOME'};
 if($USER =~ /zender/){
 # NB: area variable is not in ACME cube-sphere native output so this answer is not area-weighted
-# ncclimo -v FSNT,AODVIS -c famipc5_ne30_v0.3_00003 -s 1980 -e 1983 -i ${DATA}/ne30/raw -o ${DATA}/ne30/clm
+# ncclimo --dec_md=scd -v FSNT,AODVIS -c famipc5_ne30_v0.3_00003 -s 1980 -e 1983 -i ${DATA}/ne30/raw -o ${DATA}/ne30/clm
 # ncwa -O ${DATA}/ne30/clm/famipc5_ne30_v0.3_00003_ANN_197912_198311_climo.nc ~/foo.nc
 # ncks -H --trd -u -C -v FSNT ~/foo.nc
 	$dsc_sng="ncclimo (depends on input files in ${DATA}/ne30/raw)";
-	$tst_cmd[0]="ncclimo -v FSNT,AODVIS -c famipc5_ne30_v0.3_00003 -s 1980 -e 1983 -i ${DATA}/ne30/raw -o ${DATA}/ne30/clm > ${HOME}/foo.ncclimo";
+	$tst_cmd[0]="ncclimo --dec_md=scd -v FSNT,AODVIS -c famipc5_ne30_v0.3_00003 -s 1980 -e 1983 -i ${DATA}/ne30/raw -o ${DATA}/ne30/clm > ${HOME}/foo.ncclimo";
 	$tst_cmd[1]="ncwa -O $fl_fmt $nco_D_flg ${DATA}/ne30/clm/famipc5_ne30_v0.3_00003_ANN_197912_198311_climo.nc %tmp_fl_00%";
 	$tst_cmd[2]="ncks -O $fl_fmt $nco_D_flg -H --trd -u -C -v FSNT %tmp_fl_00%";
 	$tst_cmd[3]="FSNT = 235.965 W/m2";
