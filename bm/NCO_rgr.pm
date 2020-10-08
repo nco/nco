fb4ff776f227a07b474d5ec4b021a28b27ab4154
Charlie Zender <zender@uci.edu>
2020-10-08 15:56:15 -0700

    Replace old benchmark case famipc5_ne30_v0.3_00003 by 20180129.DECKv1b_piControl.ne30_oEC.edison

--- bm/NCO_rgr.pm
+++ bm/NCO_rgr.pm
@@ -157,21 +157,21 @@ print "\n";
     $HOME=$ENV{'HOME'};
 if($USER =~ /zender/){
 # NB: area variable is not in ACME cube-sphere native output so this answer is not area-weighted
-# ncclimo --dec_md=scd -v FSNT,AODVIS -c famipc5_ne30_v0.3_00003 -s 1980 -e 1983 -i ${DATA}/ne30/raw -o ${DATA}/ne30/clm
-# ncwa -O ${DATA}/ne30/clm/famipc5_ne30_v0.3_00003_ANN_197912_198311_climo.nc ~/foo.nc
+# ncclimo -v FSNT,AODVIS -c 20180129.DECKv1b_piControl.ne30_oEC.edison -s 1 -e 2 -i ${DATA}/ne30/raw -o ${DATA}/ne30/clm
+# ncwa -O ${DATA}/ne30/clm/20180129.DECKv1b_piControl.ne30_oEC.edison_ANN_000101_000212_climo.nc ~/foo.nc
 # ncks -H --trd -u -C -v FSNT ~/foo.nc
 	$dsc_sng="ncclimo (depends on input files in ${DATA}/ne30/raw)";
-	$tst_cmd[0]="ncclimo --dec_md=scd -v FSNT,AODVIS -c famipc5_ne30_v0.3_00003 -s 1980 -e 1983 -i ${DATA}/ne30/raw -o ${DATA}/ne30/clm > ${HOME}/foo.ncclimo";
-	$tst_cmd[1]="ncwa -O $fl_fmt $nco_D_flg ${DATA}/ne30/clm/famipc5_ne30_v0.3_00003_ANN_197912_198311_climo.nc %tmp_fl_00%";
+	$tst_cmd[0]="ncclimo -v FSNT,AODVIS -c 20180129.DECKv1b_piControl.ne30_oEC.edison -s 1 -e 2 -i ${DATA}/ne30/raw -o ${DATA}/ne30/clm > ${HOME}/foo.ncclimo";
+	$tst_cmd[1]="ncwa -O $fl_fmt $nco_D_flg ${DATA}/ne30/clm/20180129.DECKv1b_piControl.ne30_oEC.edison_ANN_000101_000212_climo.nc %tmp_fl_00%";
 	$tst_cmd[2]="ncks -O $fl_fmt $nco_D_flg -H --trd -u -C -v FSNT %tmp_fl_00%";
-	$tst_cmd[3]="FSNT = 235.965 W/m2";
+	$tst_cmd[3]="FSNT = 241.746 W/m2";
 	$tst_cmd[4]="SS_OK";
 	NCO_bm::tst_run(\@tst_cmd);
 	$#tst_cmd=0; # Reset array
 } # !zender
 
 ####################
-#### ncremap tests #
+### ncremap tests #
 ####################
     $opr_nm="ncremap";
 ####################
