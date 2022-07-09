2b4b8dc057ce783d859345976f47c1d7bfb3dda2
Charlie Zender <zender@uci.edu>
2022-07-09 12:07:49 -0700

    Rewrite nc_inq_var_filter_info() stub for old (pre-4.7.4) netCDF libraries

--- src/nco/nco_netcdf.c
+++ src/nco/nco_netcdf.c
@@ -1088,8 +1088,9 @@ int nc_inq_var_filter_info(const int nc_id,const int var_id,const unsigned int f
     (void)fprintf(stdout,"WARNING: NCO was linked to and old version (%d) of netCDF that does not support %s functionality for filters other than Shuffle, DEFLATE, and Fletcher32. However, %s was asked about information for HDF5 filter ID = %u about which it cannot answer. To obtain a fully functioning NCO that can parse this dataset, please rebuild NCO against netCDF library version 4.7.4 (released ~20200327) or later...preferably much later!\n",NC_LIB_VERSION,fnc_nm,fnc_nm);
     nco_err_exit(rcd,fnc_nm);    
     break;
-
-    return rcd;
+  } /* !flt_id */
+  
+  return rcd;
 } /* !nc_inq_var_filter_info() */
 #endif /* !474, !4.7.4 */
 
