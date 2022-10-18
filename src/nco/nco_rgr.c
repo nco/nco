b818e81b805e1f9846fa068952a461b72b7c4f63
Charlie Zender <zender@uci.edu>
2022-10-18 08:57:53 -0700

    ncremap add clause to cause --add_fll to set cells with sgs_frc == 0.0 to _FillValue

--- src/nco/nco_rgr.c
+++ src/nco/nco_rgr.c
@@ -5325,6 +5325,38 @@ nco_rgr_wgt /* [fnc] Regrid with external weights */
 	      } /* !lvl_idx */
 	    } /* !frc_out */
 	  } /* !dst_idx */
+
+	  /* 20221018: Above clause successfully sets continental gridpoints fo _FillValue in MPAS output
+	     However, it leaves cells with no local SGS contribution equal to zero instead of _FillValue
+	     In MPAS-Seaice output, for example, every ocean cell without sea ice is still 0.0 not _FillValue
+	     MPAS-Seaice output often contains a floating-point (not integer) ice presence mask timeMonthly_avg_icePresentIntMask
+	     msk_apl should help set to _FillValue any gridcells with timeMonthly_avg_icePresentIntMask == 0.0
+	     Following clause complements above clause by additionally setting to _FillValue any gridcells with zero SGS fraction */
+	  if(sgs_frc_out){
+	    for(dst_idx=0;dst_idx<grd_sz_out;dst_idx++){
+	      if(sgs_frc_out[dst_idx] == 0.0){
+		for(lvl_idx=0;lvl_idx<lvl_nbr;lvl_idx++){
+		  var_val_dbl_out[dst_idx+lvl_idx*grd_sz_out]=mss_val_cmp_dbl;
+		} /* !lvl_idx */
+	      } /* !frc_out */
+	    } /* !dst_idx */
+	  } /* !sgs_frc_out */
+
+	} /* !flg_add_fll */
+
+	if(flg_add_fll && !has_mss_val && sgs_frc_out){
+	  /* 20221018: Initialize fields without _FillValue in input file to default missing value in unmapped destination cells
+	     Otherwise empty destination cells will be zero (not _FillValue) in output file
+	     Fields with input _FillValue are already _FillValue in output where tally is zero */
+	  for(dst_idx=0;dst_idx<grd_sz_out;dst_idx++){
+	    if(frc_out[dst_idx] == 0.0){
+	      for(lvl_idx=0;lvl_idx<lvl_nbr;lvl_idx++){
+		/* 20220207: Inititalize to mss_val_cmp_dbl instead of NC_FILL_DOUBLE
+		   Fixes fields that had _FillValue attribute and no _FillValue values */
+		var_val_dbl_out[dst_idx+lvl_idx*grd_sz_out]=mss_val_cmp_dbl;
+	      } /* !lvl_idx */
+	    } /* !frc_out */
+	  } /* !dst_idx */
 	} /* !flg_add_fll */
 	
 	if(flg_msk_apl){
