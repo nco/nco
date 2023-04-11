0574376e10731012e80a5618142b31f8aed4b36a
hmb1 <henryb@hush.com>
2023-04-11 13:47:55 +0100

    custom rewrite of "class  CharScannerLiteralsLess" so that it compiles for clang16 C++17

---
+++ src/nco++/antlrCharScannerModify.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Crude patch for the include file antlr/CharScanner.hpp
+#
+# it takes the folllowing C++ code line
+#
+# class ANTLR_API CharScannerLiteralsLess : public ANTLR_USE_NAMESPACE(std)binary_function<ANTLR_USE_NAMESPACE(std)string,ANTLR_USE_NAMESPACE(std)string,bool> {
+#
+#   and removes the   std::binary_function<std::string,std::string, bool>
+#
+# class ANTLR_API CharScannerLiteralsLess {
+#
+
+if [ $# -lt 2 ];then
+  echo "usage: $0 <header-in> <header-out>"
+  exit 1;
+fi
+
+sed -E  's/:[ \t]+public ANTLR_USE_NAMESPACE\(std\)binary_function<.*>//g' <$1 >$2
