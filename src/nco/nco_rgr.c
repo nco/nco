3baccf3264f2c7284b0eef3a14263dda0c51722e
Charlie Zender <zender@uci.edu>
2023-07-23 12:08:35 -0700

    Implement nco_msh_lon_cf() in nco_rgr_wgt() to ensureCF-compliant longitude bounds in regridded files

--- src/nco/nco_rgr.c
+++ src/nco/nco_rgr.c
@@ -6108,6 +6108,9 @@ nco_rgr_wgt /* [fnc] Regrid with external weights */
   /* Begin data mode */
   (void)nco_enddef(out_id);
 
+  /* 20230722: Adjust corner longitudes to adhere to CF conventions */
+  if(flg_grd_out_1D || flg_grd_out_crv) nco_msh_lon_cf(grd_sz_out,bnd_nbr_out,lon_ctr_out,lon_bnd_out); else nco_msh_lon_cf(lon_nbr_out,bnd_nbr_out,lon_ctr_out,lon_bnd_out);
+
   /* Write new coordinates and variables to regridded file */
   if(flg_grd_out_1D){
     dmn_srt_out[0]=0L;
