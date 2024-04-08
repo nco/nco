7765e159889952352fed79d8e6824331eeddabba
Charlie Zender <zender@uci.edu>
2024-04-08 08:32:34 -0700

    add explicit cast of nco_baa_cnv_typ to nco_flt_typ_enm for C++ compilers

--- src/nco/nco_ppc.c
+++ src/nco/nco_ppc.c
@@ -1276,7 +1276,7 @@ nco_ppc_bitmask /* [fnc] Mask-out insignificant bits of significand */
   switch(type){
   case NC_FLOAT: 
   case NC_DOUBLE: 
-    (void)nco_qnt_mtd(nc_out_id,var_out_id,nco_baa_cnv_typ,(nco_flt_typ_enm)0,nsd);
+    (void)nco_qnt_mtd(nc_out_id,var_out_id,(nco_flt_typ_enm)nco_baa_cnv_typ,(nco_flt_typ_enm)0,nsd);
   break;
   case NC_INT: /* Do nothing for non-floating point types ...*/
   case NC_SHORT:
